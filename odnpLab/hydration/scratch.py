6d841e7b6558b837da871e39296c44138a81a96c
yanxianUCSB <yanxian@umail.ucsb.edu>
2020-01-30 12:14:55 -0800

    WIP/TST/DOC: docstrings added and testcases added

--- odnpLab/hydration/scratch.py
+++ odnpLab/hydration/scratch.py
@@ -2,6 +2,7 @@
 # goal: take odnpImport and output tcorr and many observables
 
 import numpy as np
+from scipy import interpolate
 
 
 def getT1p(T1: np.array, power: np.array):
@@ -12,25 +13,28 @@ def getT1p(T1: np.array, power: np.array):
     :param power: np.array of power in Watt unit, same length as T1
     :return
         a function that takes power and returns T1.
-
+        points inside the data range will be linearly interpolated
+        points outside the data range will be extrapolated
     """
-    # TODO:// Implement this
-    raise NotImplementedError
+    return interpolate.interp1d(power, T1,
+                                kind='linear',
+                                fill_value='extrapolate')
 
 
-def getT1CorrectedEnhancement(E: np.array, power: np.array):
+def getT1CorrectedEnhancement(E: np.array, power: np.array, t1p_fun: callable):
     """
     returns T1 corrected enhancements
 
     :param E: np.array of enhancements
     :param power: np.array of power in Watt unit, same length as E
+    :param t1p_fun: callable function that returns T1 at any power
     :return: Ecorr, np.array of corrected enhancements with the same length as E
     """
-    # TODO:// Implement this
+    # TODO: Implement this
     raise NotImplementedError
 
 
-def getEpsilon(E: np.array, power: np.array):
+def getKsigma(E: np.array, power: np.array):
     """
     returns k_sigma
 
@@ -39,7 +43,7 @@ def getEpsilon(E: np.array, power: np.array):
     :return:
         float k_sigma
     """
-    # TODO:// Implement this
+    # TODO: Implement this
     raise NotImplementedError
 
 
@@ -51,7 +55,16 @@ def getTcorr(eps: float):
     :return:
         float tcorr
     """
-    # TODO:// Implement this
+    # TODO: Implement this
     raise NotImplementedError
 
+
+def wip_getJH(ksi: float):
+    """
+    returns JH ksi_fit=((6*0.0063)-0.0063)/((6*0.0063)+(3*JH)+0.0063);
+    :param ksi:
+    :return:
+    """
+    # TODO: clean up
+    raise NotImplementedError
 
