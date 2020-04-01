58d7f4b00934e95026d175ab0aa1c911788fdb84
Tim Keller <tkeller@bridge12.com>
2020-04-01 08:17:51 -0400

    Fixed a but in power.py

--- odnpLab/odnpImport/power.py
+++ odnpLab/odnpImport/power.py
@@ -76,12 +76,12 @@ def assignPower(dataDict,expNumList,powersList):
         if str(expNum) in dataDict:
             if doInitialize:
                 data = dataDict[str(expNum)]
-                data.add_axes('power',powersList[ix])
+                data.addAxes('power',powersList[ix])
                 doInitialize = False
             else:
                 tempData = dataDict[str(expNum)].copy()
-                tempData.add_axes('power',powersList[ix])
-                data.concatenate_along(tempData,'power')
+                tempData.addAxes('power',powersList[ix])
+                data.concatenateAlong(tempData,'power')
 
 
     return data
