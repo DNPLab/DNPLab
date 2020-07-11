7650770529f97178f0404056e6e1d91efd60cb3d
Tim Keller <tkeller@bridge12.com>
2020-07-11 11:15:53 -0400

    Fixing merge conflcts

--- odnpLab/odnpImport/kea.py
+++ odnpLab/odnpImport/kea.py
@@ -1,26 +1,25 @@
-#import odnpData
-#from odnpData import odnpData
 from .. import odnpData
 import numpy as np
-import os
+from struct import unpack
 
 
-def importKea(path, expNum=1, verbose=False):
-    """TODO:
+def importKea(path,filename = '',num = 1 ,verbose = False):
+    '''Import Kea data
 
     Args:
-        path(str): path to experiment folder
-        expNum(int): experiment number
-        verbose(bool): whether berbose or not
-
+        path (str): Directory of data
+        filename (str): Filename of data
+        num (int): Experiment number
+        verbose (bool): If true, prints additional information for troubleshooting
+    
     Returns:
-        odnpData: data
-
-    """
+        odnpData object with Kea data
+    '''
     params_dict = {}
     try:
-        with open(os.path.join(str(path), str(expNum), 'acqu.par'), 'r') as f:
+        with open(path + filename + '/%i/'%num + 'acqu.par','r') as f:
             raw_params = f.read()
+
 
         raw_params = raw_params.strip().split('\n')
         for line in raw_params:
@@ -38,7 +37,7 @@ def importKea(path, expNum=1, verbose=False):
     except:
         pass
 
-    raw_data = np.loadtxt(os.path.join(str(path), str(expNum), 'data.csv'), delimiter =',')
+    raw_data = np.loadtxt(path + filename + '/%i/'%num + 'data.csv', delimiter = ',')
 
     t = raw_data[:,0]
     t = t / 1.e6 # convert from us to s
@@ -50,10 +49,8 @@ def importKea(path, expNum=1, verbose=False):
 #    data.kea_params = params_dict
     return data
 
-
 def importEmx(path,filename = ''):
-    '''
-    Load EMX data
+    '''Load EMX data
     '''
 
     if path[-1] != '\\' and path[-1] != '/':
@@ -98,76 +95,43 @@ def importEmx(path,filename = ''):
     output = odnpData(data,[field],['field'],paramsDict)
     return output
 
-#def saveh5(dataDict,path):
-#    '''
-#    Save All Data in .h5 format
-#    Requires a dictionary of odnpData objects
-#
-#    '''
-#
-#    if not isinstance(dataDict,dict):
-#        print('data input must be dictionary of odnpData objects')
-#        return
-#
-#    keysList = dataDict.keys()
-#
-#    f = h5py.File(path,'w')
-#    for key in keysList:
-#        odnpDataObject = dataDict[key]
-#
-#        h5_data = f.create_dataset(key,data = odnpDataObject.data)
-#        for index in range(len(odnpDataObject.axesLabels)):
-#            label = odnpDataObject.axesLabels[index]
-#            detailedLabel = '*' + key + ' ' + label + ' axes*'
-#            axes = odnpDataObject.axes[index].copy()
-#
-#            f[detailedLabel] = axes
-#            f[detailedLabel].make_scale(detailedLabel)
-#            f[key].dims[index].attach_scale(f[detailedLabel])
-#
-#            for k in odnpDataObject.params.keys():
-#                f[key].attrs[k] = odnpDataObject.params[k]
-#    f.close()
-#
-#def loadh5(path):
-#    '''
-#    Returns Dictionary of odnpDataObjects
-#    '''
-#
-#    odnpDict = {}
-#
-#    f = h5py.File(path,'r')
-#
-#    keysList = f.keys()
-#    dataKeysList = []
-#    for key in keysList:
-#        if (key[-1] == '*') and (key[0] == '*'):
-#            pass
-#            # disregard as data, assume axes
-#        else:
-#            dataKeysList.append(key)
-#    
-#    for key in dataKeysList:
-#        data = f[key][:]
-#        params =  {} 
-#        for k in f[key].attrs.keys():
-#            if k != 'DIMENSION_LIST':
-#                params[k] = f[key].attrs[k]
-#
-#        axesLabels = []
-#        axes = []
-#
-#        for index in range(len(np.shape(data))):
-#            dimKeyRaw = f[key].dims[index].keys()[0] # assumes 1 key only
-#            label = dimKeyRaw[len('*' + key + ' '):-1*len(' axes*')]
-#
-#            axesLabels.append(label)
-#            axes.append(f[key].dims[index][dimKeyRaw][:])
-#        odnpDict[key] = odnpData(data,axes,axesLabels,params)
-#    
-#
-#    f.close()
-#    return odnpDict
-#
-#
-#
+def importd01(filename):
+    '''Import Prospa Binary Format
+
+    Args:
+        filename: Path to data
+
+    Returns:
+        numpy.ndarray: binary data
+    '''
+
+    headerSize = 32
+
+    header_fmt = '<8i'
+    with open(filename,'rb') as f:
+        headerString = f.read(headerSize)
+        header = unpack(header_fmt,headerString)
+
+        data_amount = header[0]
+        data_format = header[1]
+        data_dimension = header[2]
+        array_of_dimensions = header[3:7]
+        overall_data_size = header[7]
+
+        if data_format == 0:
+            unpack_type = 'd'
+            bytes_per_point = 8
+        else:
+            unpack_type = 'f'
+            bytes_per_point = 4
+
+
+
+        total_bytes = bytes_per_point*overall_data_size
+        dataString = f.read(4)
+
+        data = unpack('<f',dataString)
+
+    return data
+
+
