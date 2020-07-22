195eb167e892632be4025c39be4d7c7307045a20
Tim Keller <tkeller@bridge12.com>
2020-07-22 08:26:53 -0400

    Updated camel case to underscore convention in dnpNMR module

--- dnpLab/dnpNMR.py
+++ dnpLab/dnpNMR.py
@@ -1,51 +1,51 @@
 from . import dnpData, dnpdata_collection
 import numpy as _np
 
-_defaultFourierTransformParameters = {
+_default_fourier_transform_parameters = {
         'dim': 't',
         'zero_fill_factor' : 2, # zero fill dim to 
         'shift' : True, # shift dim to center zero frequency
         'convert_to_ppm' : True, # convert dim to ppm
         }
 
-_defaultAlignParameters = {
+_defaultAlign_parameters = {
         'dim': 't',
         }
 
-_defaultWindowParameters = {
+_default_window_parameters = {
         'linewidth' : 10,
         'dim' : 't',
         }
 
-_defaultIntegrateParameters = {
+_default_integrate_parameters = {
         'dim' : 't',
         'integrate_center' : 0,
         'integrate_width' : 100,
         }
 
-_defaultRemoveOffsetParameters = {
+_default_remove_offset_parameters = {
         'dim' : 't',
         'offset_points' : 10,
         }
 
-def returnData(allData):
+def return_data(all_data):
     '''
     Determine what the data type is for processing
     '''
 
     isDict = False
-    if isinstance(allData,dnpData):
-        data = allData.copy()
-    elif isinstance(allData, dict):
+    if isinstance(all_data,dnpData):
+        data = all_data.copy()
+    elif isinstance(all_data, dict):
         isDict = True
-        if 'proc' in allData:
-            data = allData['proc'].copy()
-        elif 'raw' in allData:
-            data = allData['raw'].copy()
-    elif isinstance(allData, dnpdata_collection):
+        if 'proc' in all_data:
+            data = all_data['proc'].copy()
+        elif 'raw' in all_data:
+            data = all_data['raw'].copy()
+    elif isinstance(all_data, dnpdata_collection):
         isDict = True
-        if allData.processing_buffer in allData.keys():
-            data = allData[allData.processing_buffer]
+        if all_data.processing_buffer in all_data.keys():
+            data = all_data[all_data.processing_buffer]
         else:
             raise ValueError('No data in processing buffer')
     else:
@@ -53,44 +53,44 @@ def returnData(allData):
 
     return data, isDict
 
-def updateParameters(procParameters, requiredList, defaultParameters):
+def update_parameters(proc_parameters, requiredList, default_parameters):
     '''
     For all parameters without default parameters, assign parameter value to default
 
     '''
-    updatedProcParameters = procParameters
+    updatedProc_parameters = proc_parameters
     for requiredParameter in requiredList:
-        if not requiredParameter in updatedProcParameters:
-            updatedProcParameters[requiredParameter] = defaultParameters[requiredParameter]
+        if not requiredParameter in updatedProc_parameters:
+            updatedProc_parameters[requiredParameter] = default_parameters[requiredParameter]
             print('Required parameter "%s" not given.\nSetting "%s" to default value of:'%(requiredParameter,requiredParameter))
-            print(defaultParameters[requiredParameter])
+            print(default_parameters[requiredParameter])
 
-    return updatedProcParameters
+    return updatedProc_parameters
 
-def removeOffset(allData, procParameters):
+def remove_offset(all_data, proc_parameters):
     '''Remove DC offset from FID by averaging the last few data points and subtracting the average
 
     Args:
-        allData (dnpData, dict): Data container for data
-        procParameters (dict,procParam): Processing Parameters
+        all_data (dnpData, dict): Data container for data
+        proc_parameters (dict,procParam): Processing _parameters
 
     .. code-block:: python
 
-       procParameters['dim'] = 't'
-       procParameters['offset_points'] = 10
+       proc_parameters['dim'] = 't'
+       proc_parameters['offset_points'] = 10
 
-       outData = dnpLab.dnpNMR.removeOffset(allData,procParameters)
+       outData = dnpLab.dnpNMR.remove_offset(all_data,proc_parameters)
 
     Returns:
-        allData (dnpData, dict)
+        all_data (dnpData, dict)
     '''
     # Determine if data is dictionary or dnpData object
-    data, isDict = returnData(allData)
+    data, isDict = returnData(all_data)
 
-    requiredList = _defaultRemoveOffsetParameters.keys()
-    procParameters = updateParameters(procParameters,requiredList,_defaultRemoveOffsetParameters)
-    dim = procParameters['dim']
-    offset_points = int(procParameters['offset_points'])
+    requiredList = _default_remove_offset_parameters.keys()
+    proc_parameters = update_parameters(proc_parameters,requiredList,_default_remove_offset_parameters)
+    dim = proc_parameters['dim']
+    offset_points = int(proc_parameters['offset_points'])
 
     offsetData = data['t',-1*offset_points:].values
     offsetData = offsetData.reshape(-1)
@@ -99,51 +99,51 @@ def removeOffset(allData, procParameters):
     data -= offset
 
     proc_attr_name = 'remove_offset'
-    proc_dict = {k:procParameters[k] for k in procParameters if k in requiredList}
+    proc_dict = {k:proc_parameters[k] for k in proc_parameters if k in requiredList}
     data.add_proc_attrs(proc_attr_name, proc_dict)
 
     if isDict:
-        allData['proc'] = data
-        return allData
+        all_data['proc'] = data
+        return all_data
     else:
         return data
 
 
-def fourierTransform(allData, procParameters):
-    '''Perform Fourier Transform down dim dimension given in procParameters
+def fourier_transform(all_data, proc_parameters):
+    '''Perform Fourier Transform down dim dimension given in proc_parameters
 
     .. Note::
         Assumes dt = t[1] - t[0]
 
     Args:
-        allData (dnpData, dict): Data container
-        procParameters (dict, procParam): Processing parameters
+        all_data (dnpData, dict): Data container
+        proc_parameters (dict, procParam): Processing parameters
 
     Returns:
-        allData (dnpData, dict): Processed data in container
+        all_data (dnpData, dict): Processed data in container
         
     Example:
 
     .. code-block:: python
     
-        procParameters['dim'] = 't'
-        procParameters['zero_fill_factor'] = 2
-        procParameters['shift'] = True
-        procParameters['convert_to_ppm'] = True
+        proc_parameters['dim'] = 't'
+        proc_parameters['zero_fill_factor'] = 2
+        proc_parameters['shift'] = True
+        proc_parameters['convert_to_ppm'] = True
 
-        allData = dnpLab.dnpNMR.fourierTransform(allData, procParameters)
+        all_data = dnpLab.dnpNMR.fourier_transform(all_data, proc_parameters)
     '''
 
     # Determine if data is dictionary or dnpData object
-    data, isDict = returnData(allData)
+    data, isDict = returnData(all_data)
 
-    requiredList = _defaultFourierTransformParameters.keys()
-    procParameters = updateParameters(procParameters,requiredList,_defaultFourierTransformParameters)
+    requiredList = _default_fourier_transform_parameters.keys()
+    proc_parameters = update_parameters(proc_parameters,requiredList,_default_fourier_transform_parameters)
 
-    dimLabel = procParameters['dim']
-    zero_fill_factor = procParameters['zero_fill_factor']
-    shift = procParameters['shift']
-    convert_to_ppm = procParameters['convert_to_ppm']
+    dimLabel = proc_parameters['dim']
+    zero_fill_factor = proc_parameters['zero_fill_factor']
+    shift = proc_parameters['shift']
+    convert_to_ppm = proc_parameters['convert_to_ppm']
 
     index = data.dims.index(dimLabel)
     dt = data.coords[index][1] - data.coords[index][0]
@@ -162,21 +162,21 @@ def fourierTransform(allData, procParameters):
     data.coords[index] = f
 
     proc_attr_name = 'fourier_transform'
-    proc_dict = {k:procParameters[k] for k in procParameters if k in requiredList}
+    proc_dict = {k:proc_parameters[k] for k in proc_parameters if k in requiredList}
     data.add_proc_attrs(proc_attr_name, proc_dict)
 
     if isDict:
-        allData['proc'] = data
-        return allData
+        all_data['proc'] = data
+        return all_data
     else:
         return data
 
-def window(allData,procParameters):
+def window(all_data,proc_parameters):
     '''Apply Apodization to data down given dimension
     
     Args:
-        allData (dnpData, dict): data container
-        procParameters (dict, procParam): parameter values
+        all_data (dnpData, dict): data container
+        proc_parameters (dict, procParam): parameter values
 
     .. note::
         Axis units assumed to be seconds
@@ -185,21 +185,21 @@ def window(allData,procParameters):
 
     .. code-block:: python
 
-        procParameters = {
+        proc_parameters = {
                 'linewidth' : 10,
                 'dim' : 't',
                 }
-        allData = dnpLab.dnpNMR.window(allData,procParameters)
+        all_data = dnpLab.dnpNMR.window(all_data,proc_parameters)
         
     '''
 
-    data, isDict = returnData(allData)
+    data, isDict = returnData(all_data)
 
-    requiredList = _defaultWindowParameters.keys()
-    procParameters = updateParameters(procParameters,requiredList,_defaultWindowParameters)
+    requiredList = _default_window_parameters.keys()
+    proc_parameters = update_parameters(proc_parameters,requiredList,_default_window_parameters)
 
-    dimLabel = procParameters['dim']
-    linewidth = procParameters['linewidth']
+    dimLabel = proc_parameters['dim']
+    linewidth = proc_parameters['linewidth']
 
     index = data.dims.index(dimLabel)
 
@@ -212,21 +212,21 @@ def window(allData,procParameters):
     data.values *= window_array
 
     proc_attr_name = 'window'
-    proc_dict = {k:procParameters[k] for k in procParameters if k in requiredList}
+    proc_dict = {k:proc_parameters[k] for k in proc_parameters if k in requiredList}
     data.add_proc_attrs(proc_attr_name, proc_dict)
 
     if isDict:
-        allData['proc'] = data
-        return allData
+        all_data['proc'] = data
+        return all_data
     else:
         return data
 
-def integrate(allData,procParameters):
-    '''Integrate data down given dimension
+def integrate(all_data,proc_parameters):
+    '''_integrate data down given dimension
     
     Args:
-        allData (dnpData,dict): Data container
-        procParameters (dict, procParam): Processing Parameters
+        all_data (dnpData,dict): Data container
+        proc_parameters (dict, procParam): Processing Parameters
             dim_label: str
                 dimension to integrate
             integrate_center:
@@ -235,27 +235,27 @@ def integrate(allData,procParameters):
                 width of integration window
 
     Returns:
-        allData (dnpData,dict): Processed data
+        all_data (dnpData,dict): Processed data
 
     Exampe:
     .. code-block:: python
 
-        procParameters = {
+        proc_parameters = {
                     'dim' : 't',
                     'integrate_center' : 0,
                     'integrate_width' : 100,
                     }
-        dnpLab.dnpNMR.integrate(allData,procParameters)
+        dnpLab.dnpNMR.integrate(all_data,proc_parameters)
     '''
 
-    data, isDict = returnData(allData)
+    data, isDict = returnData(all_data)
 
-    requiredList = _defaultIntegrateParameters.keys()
-    procParameters = updateParameters(procParameters,requiredList,_defaultIntegrateParameters)
+    requiredList = _default_integrate_parameters.keys()
+    proc_parameters = update_parameters(proc_parameters,requiredList,_default_integrate_parameters)
 
-    dim = procParameters['dim']
-    integrate_center = procParameters['integrate_center']
-    integrate_width = procParameters['integrate_width']
+    dim = proc_parameters['dim']
+    integrate_center = proc_parameters['integrate_center']
+    integrate_width = proc_parameters['integrate_width']
 
     integrateMin = integrate_center - _np.abs(integrate_width)/2.
     integrateMax = integrate_center + _np.abs(integrate_width)/2.
@@ -265,31 +265,31 @@ def integrate(allData,procParameters):
     data.sum(dim)
 
     proc_attr_name = 'integrate'
-    proc_dict = {k:procParameters[k] for k in procParameters if k in requiredList}
+    proc_dict = {k:proc_parameters[k] for k in proc_parameters if k in requiredList}
     data.add_proc_attrs(proc_attr_name, proc_dict)
 
     if isDict:
-        allData['proc'] = data
-        return allData
+        all_data['proc'] = data
+        return all_data
     else:
         return data
 
 
-def align(allData,procParameters):
+def align(all_data,proc_parameters):
     '''
     Alignment of NMR spectra down given dim dimension
     '''
 
-    data, isDict = returnData(allData)
+    data, isDict = returnData(all_data)
 
     if len(_np.shape(data.values)) != 2:
         print('Only 2-dimensional data supported')
         return
 
-    requiredList = _defaultAlignParameters.keys()
-    procParameters = updateParameters(procParameters,requiredList,_defaultAlignParameters)
+    requiredList = _defaultAlign_parameters.keys()
+    proc_parameters = update_parameters(proc_parameters,requiredList,_defaultAlign_parameters)
 
-    alignAxesLabel = procParameters['dim']
+    alignAxesLabel = proc_parameters['dim']
     originalAxesOrder = data.dims
     data.reorder(alignAxesLabel)
     dimIter = data.dims[-1]
@@ -305,11 +305,11 @@ def align(allData,procParameters):
     data.reorder(originalAxesOrder)
 
     proc_attr_name = 'align'
-    proc_dict = {k:procParameters[k] for k in procParameters if k in requiredList}
+    proc_dict = {k:proc_parameters[k] for k in proc_parameters if k in requiredList}
     data.add_proc_attrs(proc_attr_name, proc_dict)
 
     if isDict:
-        allData['proc'] = data
-        return allData
+        all_data['proc'] = data
+        return all_data
     else:
         return data
