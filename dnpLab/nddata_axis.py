67614553a19ed97de5d6df6cae4fd6013a43a9dd
Timothy Keller <tkeller@bridge12.com>
2020-07-23 10:41:43 -0400

    Updating nddata_axis properties, adding __add__ and reduce functions

--- dnpLab/nddata_axis.py
+++ dnpLab/nddata_axis.py
@@ -11,10 +11,7 @@ class nddata_axis(object):
             dim (str): name of dimension
         '''
 
-        if isinstance(dim, str):
-            self.__dim = dim
-        else:
-            raise ValueError('dim must be type str not %s'%(str(type(dim))))
+        self.dim = dim
 
         if isinstance(coord, np.ndarray):
             self.__coord = coord
@@ -23,7 +20,39 @@ class nddata_axis(object):
             self.__stop = coord.stop
             self.__step = coord.step
 
-        self.__size = self.array.size
+#        self.__size = self.array.size
+    
+    def reduce(self):
+        '''
+        '''
+
+        if hasattr(self, '_array'):
+            start = self._array[0]
+            step = self._array[1] - self._array[0]
+            stop = self._array[-1] + step
+
+            if np.allclose(self._array, np.r_[slice(start,stop,step)]):
+                self.start = start
+                self.stop = stop
+                self.step = step
+            else:
+                raise ValueError('Array must have evenly spaced values to be reduced')
+        else:
+            raise ValueError('No array to reduce')
+
+    @property
+    def dim(self):
+        return self.__dim
+
+    @dim.setter
+    def dim(self, dim):
+        '''
+        '''
+
+        if isinstance(dim, str):
+            self.__dim = dim
+        else:
+            raise TypeError('dim must be type str not %s'%str(type(dim)))
 
     @property
     def start(self):
@@ -31,17 +60,38 @@ class nddata_axis(object):
         '''
         return self.__start
 
+    @start.setter
+    def start(self, b):
+        '''
+        '''
+        if isinstance(b, (int, float)):
+            self.__start = b
+
     @property
     def stop(self):
         '''
         '''
         return self.__stop
 
+    @stop.setter
+    def stop(self, b):
+        '''
+        '''
+        if isinstance(b, (int, float)):
+            self.__stop = b
+
     @property
     def step(self):
         '''
         '''
         return self.__step
+
+    @step.setter
+    def step(self, b):
+        '''
+        '''
+        if isinstance(b, (int, float)):
+            self.__step = b
 
     @property
     def array(self):
@@ -50,19 +100,33 @@ class nddata_axis(object):
 
         # Fast method
 #        try:
-#            return self.__array
+#            return self._array
 #        except:
-#            self.__array = np.r_[slice(self.start, self.stop, self.step)]
-#            return self.__array
+#            self._array = np.r_[slice(self.start, self.stop, self.step)]
+#            return self._array
         # 2nd Attempt at fast method
-        if hasattribute(self, '__array'):
-            return self.__array
-        except:
-            self.__array = np.r_[slice(self.start, self.stop, self.step)]
-            return self.__array
+        if hasattr(self, '_array'):
+            return self._array
+        else:
+            self._array = np.r_[slice(self.start, self.stop, self.step)]
+            return self._array
 
 #        # Slow method, 56 times slower
 #        return np.r_[slice(self.start, self.stop, self.step)]
+
+    @array.setter
+    def array(self, b):
+        '''
+        '''
+        if isinstance(b, np.ndarray):
+            self._array = b
+        else:
+            raise TypeError('array type must be numpy.ndarray')
+           
+
+    @array.deleter
+    def array(self):
+        del self._array
 
     def slice(self, *args):
         '''
@@ -78,7 +142,7 @@ class nddata_axis(object):
     def _del_array(self):
         '''
         '''
-        del self.__array
+        del self._array
 
 
     @property
@@ -87,6 +151,22 @@ class nddata_axis(object):
         '''
         return self.array.size # 3 times faster when array is stored in object
 #        return max(int((self.stop - self.start) / self.step),0) # Take max for case when direction is reversed
+    def __repr__(self):
+        return 'nddata_axis(\'{}\', {})'.format(self.dim,self.array)
+
+    def __str__(self):
+        return '\'{}\':\n{}'.format(self.dim,str(self.array))
+    
+    def __add__(self, b):
+        start = self.start + b
+        stop = self.stop + b
+        step = self.step
+
+        if hasattr(self, '_array'):
+            del self.array
+
+        return nddata_axis(self.dim, slice(start, stop, step))
+
 
 
 if __name__ == '__main__':
