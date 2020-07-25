6cab9daa699f29a960138ae996097451b5f3ccf4
Tim Keller <tkeller@bridge12.com>
2020-07-25 16:49:53 -0400

    Added ufunc universal functions for nddata class

---
+++ dnpLab/core/defaults.py
@@ -0,0 +1,8 @@
+# Default processing parameters
+
+_fourier_transform = {
+        'dim': 't',
+        'zero_fill_factor' : 2, # zero fill dim to 
+        'shift' : True, # shift dim to center zero frequency
+        'convert_to_ppm' : True, # convert dim to ppm
+        }
