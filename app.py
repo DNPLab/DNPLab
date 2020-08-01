9769414bacaad5ce493d07a88c59480e4911aa90
Yanxian Lin <yanxian@umail.ucsb.edu>
2020-07-31 20:27:49 -0700

    APP: App is now able to take customized parameters

--- app.py
+++ app.py
@@ -25,40 +25,79 @@ def get_table_download_link(temp_file_path, filename='results'):
     return href
 
 
-def run(uploaded_file):
-    print(f"You just upload this file -> {uploaded_file}")
-    print(f"But I am in a demo mode and not going to run it actually")
+def set_ppar(ppar:ProcParameter):
+    """Prompt for users to choose parameters
 
-    # upzip
-    with zipfile.ZipFile(uploaded_file, "r") as zip_ref:
-        odnpdir = tempfile.mkdtemp(dir = TEMPDIR)
-        # After extraction, a lot of OS unrelated folders will be there
-        zip_ref.extractall(odnpdir)
-        expname = zip_ref.namelist()[0]
-        odnppath = os.path.join(odnpdir, expname)
+    Returns: tuple(ProcParameter, HydrationParameter)
 
-    """Process CNSI ODNP and return a str of results"""
-    path = odnppath  # path to CNSI data folder
-    ppar = ProcParameter()
-    hpar = HydrationParameter()
+    """
+    # Processing parameter
+    st.write("Enhancement integration")
+    ppar.eic = st.number_input("window center", value=ppar.eic, step=1, key='eic')
+    ppar.eiw = st.number_input("window width", value=ppar.eiw, step=1, key='eiw')
+    st.write("T1 integration")
+    ppar.tic = st.number_input("window center", value=ppar.tic, step=1, key='tic')
+    ppar.tiw = st.number_input("window width", value=ppar.tiw, step=1, key='tiw')
 
-    ppar.verbose = False
+    return ppar
 
-    rest = process_cnsi(path, ppar)
-    t1, t1_power, e, e_power = rest['T1p'], rest['T1powers'], rest['Ep'], rest['Epowers']
-    print(t1_power)
 
-    hpar.slC = 500
-    hpar.T10 = rest['T10']
-    hpar.field = 380.4
+def set_hpar(hpar:HydrationParameter):
+    """Prompt for users to choose parameters
 
-    hc = HydrationCalculator(T1=t1, T1_power=t1_power, E=e, E_power=e_power, hp=hpar)
-    hc.run()
-    print(hc.results)
+    Returns: tuple(ProcParameter, HydrationParameter)
 
-    mydict = {k:v for k, v in hc.results.__dict__.items() if type(v) != type(np.ndarray([]))}
-    mydict.update({k: ', '.join([f"{vi:.4f}" for vi in v]) for k, v in hc.results.__dict__.items() if
-                   type(v) == type(np.ndarray([]))})
+    """
+    # Processing parameter
+    st.write("Enhancement integration")
+    hpar.spin_C = st.number_input("Spin label concentration (uM)", value=500.0, step=1.0, key='spin_C')
+    hpar.field = st.number_input("Field (mT)", value=hpar.field, step=1.0, key='field')
+
+    if st.button('More'):
+        st.write("No more")
+
+    return hpar
+
+
+def run(uploaded_file, ppar:ProcParameter, hpar:HydrationParameter):
+    """
+
+    Args:
+        uploaded_file: zip file object
+
+    Returns: tuple(dict, str)
+        mydict: dictionary of results
+        expname: name of the experiment
+
+    """
+    # print(f"You just upload this file -> {uploaded_file}")
+    # print(f"But I am in a demo mode and not going to run it actually")
+
+    with tempfile.TemporaryDirectory(dir=TEMPDIR) as tmpdir:
+
+        # upzip to tmpdir
+        with zipfile.ZipFile(uploaded_file, "r") as zip_ref:
+            zip_ref.extractall(tmpdir)
+            expname = zip_ref.namelist()[0]
+
+        # Process CNSI ODNP and return a str of results
+        path = os.path.join(tmpdir, expname)  # path to CNSI data folder
+
+        rest = process_cnsi(path, ppar)
+
+        t1, t1_power, e, e_power = rest['T1p'], rest['T1powers'], rest['Ep'], rest['Epowers']
+
+        hpar.T10 = rest['T10']
+
+        hc = HydrationCalculator(T1=t1, T1_power=t1_power, E=e, E_power=e_power, hp=hpar)
+        hc.run()
+
+        # Create dictionary of results
+        mydict = {k:v for k, v in hc.results.__dict__.items()
+                  if type(v) != type(np.ndarray([]))}
+        mydict.update({k: ', '.join([f"{vi:.4f}" for vi in v])
+                       for k, v in hc.results.__dict__.items()
+                       if type(v) == type(np.ndarray([]))})
 
     return mydict, expname
 
@@ -67,10 +106,7 @@ def dict_to_str(mydict):
     mylist = [f"{k} \t {v}" for k, v in mydict.items()]
     return '\n'.join(mylist)
 
-#
-#
-#
-#
+
 # =======THE APP=======
 st.title('ODNPLab \n One-step ODNP processing')
 st.markdown("""
@@ -99,12 +135,22 @@ my_odnp_exp/
 
 st.markdown("## Upload a Zip file")
 uploaded_file = st.file_uploader("Here ->", type="zip")
+
 if uploaded_file is not None:
+
+    # Parameters
+    ppar = ProcParameter()
+    ppar.verbose = False
+    ppar = set_ppar(ppar)
+
+    hpar = HydrationParameter()
+    hpar = set_hpar(hpar)
+
     if st.button("Run"):
-        results, expname = run(uploaded_file)
+
+        results, expname = run(uploaded_file, ppar=ppar, hpar=hpar)
         st.write(results)
         st.markdown(get_table_download_link(dict_to_str(results), filename=expname),
                     unsafe_allow_html=True)
     else:
         st.write("^ Click Me ")
-
