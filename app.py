9d579ef503d1c27375a31c28d1e2ce0b360eed39
Yanxian Lin <yanxian@umail.ucsb.edu>
2020-07-31 19:03:52 -0700

    APP: The app now is able to process data, print results and offer a downloadable link.

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import streamlit as st
+import numpy as np
 import zipfile
 import tempfile
 import pprint
 import os
-from dnpLab.examples.workupCNSI import ProcParameter,
+import csv
+import base64
+from odnpLab.examples.workupCNSI import ProcParameter, process_cnsi
+from odnpLab.hydration import HydrationParameter, HydrationCalculator
+
 print = pprint.pprint
 
 # TEMPDIR = '/tmp/odnplab/'
 TEMPDIR = None
+
+
+def get_table_download_link(temp_file_path, filename='results'):
+    """Generates a link allowing a temp_file_path to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    b64 = base64.b64encode(temp_file_path.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.txt">Download Results</a>'
+    return href
+
 
 def run(uploaded_file):
     print(f"You just upload this file -> {uploaded_file}")
@@ -15,23 +31,41 @@ def run(uploaded_file):
 
     # upzip
     with zipfile.ZipFile(uploaded_file, "r") as zip_ref:
-        # print(f"zipfile filename {zip_ref.filename}")
-        # print(f"zipfile filelist {zip_ref.filelist}")
-        # print(f"zipfile namelist {zip_ref.namelist()}")
         odnpdir = tempfile.mkdtemp(dir = TEMPDIR)
-
         # After extraction, a lot of OS unrelated folders will be there
         zip_ref.extractall(odnpdir)
+        expname = zip_ref.namelist()[0]
+        odnppath = os.path.join(odnpdir, expname)
 
-        odnppath = os.path.join(odnpdir, zip_ref.namelist()[0])
-        print(f"odnppath= {odnppath}")
+    """Process CNSI ODNP and return a str of results"""
+    path = odnppath  # path to CNSI data folder
+    ppar = ProcParameter()
+    hpar = HydrationParameter()
+
+    ppar.verbose = False
+
+    rest = process_cnsi(path, ppar)
+    t1, t1_power, e, e_power = rest['T1p'], rest['T1powers'], rest['Ep'], rest['Epowers']
+    print(t1_power)
+
+    hpar.slC = 500
+    hpar.T10 = rest['T10']
+    hpar.field = 380.4
+
+    hc = HydrationCalculator(T1=t1, T1_power=t1_power, E=e, E_power=e_power, hp=hpar)
+    hc.run()
+    print(hc.results)
+
+    mydict = {k:v for k, v in hc.results.__dict__.items() if type(v) != type(np.ndarray([]))}
+    mydict.update({k: ', '.join([f"{vi:.4f}" for vi in v]) for k, v in hc.results.__dict__.items() if
+                   type(v) == type(np.ndarray([]))})
+
+    return mydict, expname
 
 
-    # process
-
-    # return
-
-    pass
+def dict_to_str(mydict):
+    mylist = [f"{k} \t {v}" for k, v in mydict.items()]
+    return '\n'.join(mylist)
 
 #
 #
@@ -67,7 +101,10 @@ st.markdown("## Upload a Zip file")
 uploaded_file = st.file_uploader("Here ->", type="zip")
 if uploaded_file is not None:
     if st.button("Run"):
-        run(uploaded_file)
+        results, expname = run(uploaded_file)
+        st.write(results)
+        st.markdown(get_table_download_link(dict_to_str(results), filename=expname),
+                    unsafe_allow_html=True)
     else:
         st.write("^ Click Me ")
 
