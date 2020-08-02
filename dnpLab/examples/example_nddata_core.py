600b53eb70cc88b853fc1a5fee3f166b7271dff0
Tim Keller <tkeller@bridge12.com>
2020-08-02 10:56:09 -0400

    Adding nddata_core example.

---
+++ dnpLab/examples/example_nddata_core.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../..')
+
+from dnpLab.core.nddata import nddata_core
+import dnpLab as dnp
+
+import numpy as np
+
+x = np.r_[1:10]
+values = np.random.randn(len(x))
+
+data = nddata_core(values, ['x'], [x])
+
+dnp.plot(data)
+dnp.show()
+print(data)
+
+
