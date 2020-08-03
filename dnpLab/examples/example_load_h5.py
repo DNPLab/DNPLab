fb16714a5d917c96f32af55f1f4773ad9dde7797
Tim Keller <tkeller@bridge12.com>
2020-08-03 13:39:04 -0400

    Adding examples for saving and loading h5 files.

---
+++ dnpLab/examples/example_load_h5.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../..')
+import numpy as np
+
+import dnpLab as dnp
+
+filename = 'test.h5'
+
+ws = dnp.dnpImport.h5.loadh5(filename)
+
+dnp.plot(ws['ft'])
+dnp.xlim(20,-20)
+dnp.figure()
+dnp.plot(ws['proc'], 'o')
+
+dnp.show()
+
