fbf7e7d45d1a767fa17d9ec705a4b709e30c06a3
Tim Keller <tkeller@bridge12.com>
2020-08-07 10:56:05 -0400

    Updated example of prospa dir import to now loop over files in directory.

--- examples/example_dir_prospa.py
+++ examples/example_dir_prospa.py
@@ -8,16 +8,16 @@ path = '../data/prospa/toluene_10mM_Tempone/'
 exp_list = [str(x) for x in range(1,43)]
 #exp_list = ['1','2','3','4']
 power_list = 10.**(np.r_[0:40:len(exp_list)*1j] / 10) / 1000.
-ws = dnp.dnpImport.prospa.import_prospa_dir(path, exp_list)
 
 for ix, exp_num in enumerate(exp_list):
-    tmp = ws[exp_num]
+    tmp = dnp.dnpImport.prospa.import_prospa(path + '%s'%exp_num)
     tmp.new_dim('power', power_list[ix])
     if ix == 0:
         data = tmp
     else:
         data.concatenate(tmp, 'power')
 
+ws = dnp.create_workspace()
 ws.add('raw', data)
 ws.copy('raw', 'proc')
 
