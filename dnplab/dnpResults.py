f2c189b43b004148a4d57ae720b8e75943a7da71
Tim Keller <tkeller@bridge12.com>
2020-10-09 09:36:40 -0400

    Update dnpResults.py
    Reformatted

--- dnplab/dnpResults.py
+++ dnplab/dnpResults.py
@@ -56,8 +56,9 @@ def imshow(data, *args, **kwargs):  # TODO: drop unused args and kwargs
     plt.xlabel(dims[1])
     plt.ylabel(dims[0])
 
+
 def stack(data, *args, offset=None, **kwargs):
-    '''Stack Plot for 2D data
+    """Stack Plot for 2D data
 
     Args:
         data (dnpdata): dnpdata object for matplotlib plot function
@@ -71,7 +72,7 @@ def stack(data, *args, offset=None, **kwargs):
        dnp.dnpResults.stack(data)
        dnp.dnpResults.plt.show()
 
-    '''
+    """
 
     coord = data.coords[0]
     dim = data.dims[0]
@@ -79,21 +80,35 @@ def stack(data, *args, offset=None, **kwargs):
     if offset == None:
         offset = np.max(data.abs)
 
-    offset_matrix = offset * np.ones(coord.size).reshape(-1, 1) * np.r_[0:data.coords[1].size]
+    offset_matrix = (
+        offset * np.ones(coord.size).reshape(-1, 1) * np.r_[0 : data.coords[1].size]
+    )
 
     plt.plot(coord, data.values + offset_matrix, *args, **kwargs)
 
 
 def waterfall(data, dx, dy, *args, **kwargs):
-    '''Waterfall plot for 2d data
-    '''
+    """Waterfall plot for 2d data"""
 
     coord = data.coords[0]
     dim = data.dims[0]
 
     for ix in range(data.coords[data.dims[1]].size):
-        plt.plot(coord + (ix * dx), data[data.dims[1],ix].values.ravel() + (ix * dy), *args, **kwargs, zorder = -1*ix)
-        plt.fill_between(coord + (ix * dx), data[data.dims[1],ix].values.ravel() + (ix * dy), ix * dy, facecolor = 'w', edgecolor = "None", zorder = -1*ix)
+        plt.plot(
+            coord + (ix * dx),
+            data[data.dims[1], ix].values.ravel() + (ix * dy),
+            *args,
+            **kwargs,
+            zorder=-1 * ix
+        )
+        plt.fill_between(
+            coord + (ix * dx),
+            data[data.dims[1], ix].values.ravel() + (ix * dy),
+            ix * dy,
+            facecolor="w",
+            edgecolor="None",
+            zorder=-1 * ix,
+        )
 
 
 def plot(data, *args, **kwargs):
