db84b0680697790c79790907e92aa797a3d76b29
Tim Keller <tkeller@bridge12.com>
2020-10-11 11:48:46 -0400

    Reformatted to PEP8

--- dnplab/dnpIO.py
+++ dnplab/dnpIO.py
@@ -1,7 +1,8 @@
 from . import dnpImport
 
-def import_data(path, data_type = None, *args, **kwargs):
-    '''Import data from different spectrometer formats
+
+def import_data(path, data_type=None, *args, **kwargs):
+    """Import data from different spectrometer formats
 
     Args:
         path (str): Path to data directory or file
@@ -9,7 +10,7 @@ def import_data(path, data_type = None, *args, **kwargs):
 
     Returns:
         data (dnpData): Data object
-    '''
+    """
 
     if data_type == None:
         data_type = autodetect(path)
@@ -17,21 +18,20 @@ def import_data(path, data_type = None, *args, **kwargs):
     if data_type == NotImplemented:
         return NotImplemented
 
-    if data_type == 'prospa':
+    if data_type == "prospa":
         return dnpImport.prospa.import_prospa(path, *args, **kwargs)
 
-    elif data_type == 'topspin':
+    elif data_type == "topspin":
         return dnpImport.topspin.import_topspin(path, *args, **kwargs)
 
-    elif data_type == 'topspin dir':
+    elif data_type == "topspin dir":
         return dnpImport.topspin.import_topspin_dir(path, *args, **kwargs)
 
-    elif data_type == 'vnmrj':
+    elif data_type == "vnmrj":
         return dnpImport.vnmrj.import_vnmrj(path, *args, **kwargs)
 
 
 def autodetect(path):
-    '''Automatically detect type of data in directory
-    '''
+    """Automatically detect type of data in directory"""
 
     return NotImplemented
--- unittests/test_import_wrapper.py
+++ unittests/test_import_wrapper.py
@@ -1,15 +1,15 @@
 import unittest
 from dnplab import import_data
 
+
 class import_data_tester(unittest.TestCase):
     def setUp(self):
         self.topspin_dir = "./data/topspin/"
         self.prospa_dir = "./data/prospa/toluene_10mM_Tempone"
         self.vnmrj_dir = "./data/vnmrj/10mM_tempol_in_water_array.fid"
 
-
     def test_topspin(self):
-        data = import_data(self.topspin_dir, "topspin", 1) # last arg is expNum
+        data = import_data(self.topspin_dir, "topspin", 1)  # last arg is expNum
         self.assertEqual(data.dims[0], "t2")
         self.assertEqual(data.values.size, 8147)
         self.assertAlmostEqual(data.values.min(), -5 - 4.168734491315137j)
@@ -21,7 +21,7 @@ class import_data_tester(unittest.TestCase):
     def test_vnmrj(self):
         import_data(self.vnmrj_dir, "vnmrj")
 
+
 if __name__ == "__main__":
     unittest.main()
     pass
-
