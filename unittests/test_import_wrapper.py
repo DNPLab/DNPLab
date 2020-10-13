2ccaef23732828123f106d220a1e23c2fa64c8d9
Tim Keller <tkeller@bridge12.com>
2020-10-13 13:03:53 -0400

    Update test_import_wrapper.py
    Updated name of import wrapper

--- unittests/test_import_wrapper.py
+++ unittests/test_import_wrapper.py
@@ -1,25 +1,25 @@
 import unittest
-from dnplab import import_data
+from dnplab import load
 
 
-class import_data_tester(unittest.TestCase):
+class load_wrapper_tester(unittest.TestCase):
     def setUp(self):
         self.topspin_dir = "./data/topspin/"
         self.prospa_dir = "./data/prospa/toluene_10mM_Tempone"
         self.vnmrj_dir = "./data/vnmrj/10mM_tempol_in_water_array.fid"
 
     def test_topspin(self):
-        data = import_data(self.topspin_dir, "topspin", 1)  # last arg is expNum
+        data = load(self.topspin_dir, "topspin", 1)  # last arg is expNum
         self.assertEqual(data.dims[0], "t2")
         self.assertEqual(data.values.size, 8147)
         self.assertAlmostEqual(data.values.min(), -5 - 4.168734491315137j)
         self.assertAlmostEqual(data.attrs["nmr_frequency"], 14831413.270000001)
 
     def test_prospa(self):
-        import_data(self.prospa_dir + "/1/data.csv", "prospa")
+        load(self.prospa_dir + "/1/data.csv", "prospa")
 
     def test_vnmrj(self):
-        import_data(self.vnmrj_dir, "vnmrj")
+        load(self.vnmrj_dir, "vnmrj")
 
 
 if __name__ == "__main__":
