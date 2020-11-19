1313617f2a412dd6da1e9c98d9e42d3f9679a76f
Thomas Casey <thcasey3@gmail.com>
2020-11-18 18:07:15 -0800

    added "places" arg to assertalmostequal

--- unittests/test_dnpFit.py
+++ unittests/test_dnpFit.py
@@ -29,17 +29,21 @@ class dnpFit_tester(unittest.TestCase):
             self.ws, dim="t2", integrate_center=0, integrate_width=50
         )
         efit.exponentialFit(self.ws, type="T1")
-        self.assertAlmostEqual(self.ws["fit"].attrs["T1"], 2.140702947551208)
+        self.assertAlmostEqual(self.ws["fit"].attrs["T1"], 2.140702947551208, places=4)
 
         efit.exponentialFit(self.ws, type="T2")
-        self.assertAlmostEqual(self.ws["fit"].attrs["T2"], 1.0682212598985381)
+        self.assertAlmostEqual(self.ws["fit"].attrs["T2"], 1.0682212598985381, places=4)
 
         efit.exponentialFit(self.ws, type="T2", stretched=True)
-        self.assertAlmostEqual(self.ws["fit"].attrs["T2"], 0.8938213879865939)
+        self.assertAlmostEqual(self.ws["fit"].attrs["T2"], 0.8938213879865939, places=4)
 
         efit.exponentialFit(self.ws, type="mono")
-        self.assertAlmostEqual(self.ws["fit"].attrs["tau"], 2.140702798915825)
+        self.assertAlmostEqual(self.ws["fit"].attrs["tau"], 2.140702798915825, places=4)
 
         efit.exponentialFit(self.ws, type="bi")
-        self.assertAlmostEqual(self.ws["fit"].attrs["tau1"], 2.1867254920126586)
-        self.assertAlmostEqual(self.ws["fit"].attrs["tau2"], 253552.51805618292)
+        self.assertAlmostEqual(
+            self.ws["fit"].attrs["tau1"], 2.1867254920126586, places=4
+        )
+        self.assertAlmostEqual(
+            self.ws["fit"].attrs["tau2"], 253552.51805618292, places=4
+        )
--- unittests/test_dnpNMR.py
+++ unittests/test_dnpNMR.py
@@ -19,69 +19,72 @@ class dnpNMR_tester(unittest.TestCase):
     def test_basic_nmr_processing(self):
         shape_data = np.shape(self.ws)
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 1].real), 5.447918664876271
+            max(self.ws["proc"].values[:, 1].real), 5.447918664876271, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 1].real), -4.128415225672084
+            min(self.ws["proc"].values[:, 1].real), -4.128415225672084, places=4
         )
 
         nmr.remove_offset(self.ws)
         self.assertAlmostEqual(shape_data, np.shape(self.ws))
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 1].real), 5.445267326190019
+            max(self.ws["proc"].values[:, 1].real), 5.445267326190019, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 1].real), -4.131066564358336
+            min(self.ws["proc"].values[:, 1].real), -4.131066564358336, places=4
         )
 
         self.ws.copy("proc", "temp")
         nmr.window(self.ws, type="hamming")
-        self.assertAlmostEqual(max(self.ws["proc"].attrs["window"]), 1.0)
+        self.assertEqual(max(self.ws["proc"].attrs["window"]), 1.0)
         self.assertAlmostEqual(
-            min(self.ws["proc"].attrs["window"]), 0.07671999999999995
+            min(self.ws["proc"].attrs["window"]), 0.07671999999999995, places=4
         )
 
         self.ws.copy("temp", "proc")
         self.ws = nmr.window(self.ws, type="hann")
-        self.assertAlmostEqual(max(self.ws["proc"].attrs["window"]), 1.0)
-        self.assertAlmostEqual(min(self.ws["proc"].attrs["window"]), 0.0)
+        self.assertEqual(max(self.ws["proc"].attrs["window"]), 1.0)
+        self.assertEqual(min(self.ws["proc"].attrs["window"]), 0.0)
 
         self.ws.copy("temp", "proc")
         nmr.window(self.ws, type="lorentz_gauss", linewidth=5, gauss_linewidth=10)
-        self.assertAlmostEqual(max(self.ws["proc"].attrs["window"]), 1.1895922020471337)
+        self.assertAlmostEqual(
+            max(self.ws["proc"].attrs["window"]), 1.1895922020471337, places=4
+        )
 
         self.ws.copy("temp", "proc")
         self.ws = nmr.window(
             self.ws, type="hamming", linewidth=5, gauss_linewidth=10, inverse=True
         )
-        self.assertAlmostEqual(max(self.ws["proc"].attrs["window"]), 13.03441084462983)
+        self.assertAlmostEqual(
+            max(self.ws["proc"].attrs["window"]), 13.03441084462983, places=4
+        )
         self.ws.copy("temp", "proc")
         self.ws = nmr.window(self.ws, type="sin2")
-        self.assertAlmostEqual(max(self.ws["proc"].attrs["window"]), 1.0)
-        self.assertAlmostEqual(
-            min(self.ws["proc"].attrs["window"]), 3.749399456654644e-33
-        )
+        self.assertEqual(max(self.ws["proc"].attrs["window"]), 1.0)
 
         self.ws.copy("temp", "proc")
         nmr.window(self.ws, type="exponential", linewidth=5)
         self.assertAlmostEqual(
-            min(self.ws["proc"].attrs["window"]), 0.00035733315645396175
+            min(self.ws["proc"].attrs["window"]), 0.00035733315645396175, places=4
         )
         self.ws.pop("temp")
         self.assertAlmostEqual(shape_data, np.shape(self.ws))
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 1].real), 5.390978190372195
+            max(self.ws["proc"].values[:, 1].real), 5.390978190372195, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 1].real), -3.9647928998767163
+            min(self.ws["proc"].values[:, 1].real), -3.9647928998767163, places=4
         )
 
         nmr.fourier_transform(self.ws, zero_fill_factor=2)
-        self.assertAlmostEqual(shape_data, np.shape(self.ws))
-        self.assertAlmostEqual(len(self.ws["proc"].values[:, 4].real), 15844)
-        self.assertAlmostEqual(max(self.ws["proc"].values[:, 4].real), 61.3994271072369)
+        self.assertEqual(shape_data, np.shape(self.ws))
+        self.assertEqual(len(self.ws["proc"].values[:, 4].real), 15844)
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 4].real), -76.53300374124703
+            max(self.ws["proc"].values[:, 4].real), 61.3994271072369, places=4
+        )
+        self.assertAlmostEqual(
+            min(self.ws["proc"].values[:, 4].real), -76.53300374124703, places=4
         )
 
         self.ws.copy("proc", "temp")
@@ -95,14 +98,14 @@ class dnpNMR_tester(unittest.TestCase):
             force_positive=False,
         )
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 3].real), 242.36299886442168
+            max(self.ws["proc"].values[:, 3].real), 242.36299886442168, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 3].real), -65.59778997862813
+            min(self.ws["proc"].values[:, 3].real), -65.59778997862813, places=4
         )
         phs0 = self.ws["proc"].attrs["phase_0"]
         phs1 = self.ws["proc"].attrs["phase_1"]
-        self.assertAlmostEqual(len(phs1), len(self.ws["proc"].values))
+        self.assertEqual(len(phs1), len(self.ws["proc"].values))
         self.ws.copy("proc", "keep")
         self.ws.copy("temp", "proc")
 
@@ -114,10 +117,10 @@ class dnpNMR_tester(unittest.TestCase):
             force_positive=True,
         )
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 7].real), 398.66277628048533
+            max(self.ws["proc"].values[:, 7].real), 398.66277628048533, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 7].real), -47.32339420356964
+            min(self.ws["proc"].values[:, 7].real), -47.32339420356964, places=4
         )
         self.ws.copy("temp", "proc")
 
@@ -130,10 +133,10 @@ class dnpNMR_tester(unittest.TestCase):
             phase=phs1 * (45 * np.pi / 180),
         )
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 3].real), 239.3422920589204
+            max(self.ws["proc"].values[:, 3].real), 239.3422920589204, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 3].real), -64.37971134351001
+            min(self.ws["proc"].values[:, 3].real), -64.37971134351001, places=4
         )
         self.ws.copy("temp", "proc")
 
@@ -144,30 +147,34 @@ class dnpNMR_tester(unittest.TestCase):
             phase=phs0 * (45 * np.pi / 180),
         )
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 3].real), 238.27990936049952
+            max(self.ws["proc"].values[:, 3].real), 238.27990936049952, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 3].real), -66.28951430959444
+            min(self.ws["proc"].values[:, 3].real), -66.28951430959444, places=4
         )
         self.ws.pop("temp")
         self.ws.copy("keep", "proc")
         self.ws.pop("keep")
 
         self.ws = nmr.baseline(self.ws, type="poly", order=1, reference_slice=None)
-        self.assertAlmostEqual(shape_data, np.shape(self.ws))
+        self.assertEqual(shape_data, np.shape(self.ws))
         self.assertAlmostEqual(
-            max(self.ws["proc"].values[:, 2].real), 351.9904580259595
+            max(self.ws["proc"].values[:, 2].real), 351.9904580259595, places=4
         )
         self.assertAlmostEqual(
-            min(self.ws["proc"].values[:, 2].real), -73.68315636898302
+            min(self.ws["proc"].values[:, 2].real), -73.68315636898302, places=4
         )
         self.assertAlmostEqual(
             len(self.ws["proc"].attrs["baseline"]), len(self.ws["proc"].values)
         )
         nmr.integrate(self.ws, dim="t2", integrate_center=0, integrate_width=50)
-        self.assertAlmostEqual((8,), np.shape(self.ws["proc"].values))
-        self.assertAlmostEqual(max(self.ws["proc"].values.real), 2031.4439405548092)
-        self.assertAlmostEqual(min(self.ws["proc"].values.real), -1655.6511804282302)
+        self.assertEqual((8,), np.shape(self.ws["proc"].values))
+        self.assertAlmostEqual(
+            max(self.ws["proc"].values.real), 2031.4439405548092, places=4
+        )
+        self.assertAlmostEqual(
+            min(self.ws["proc"].values.real), -1655.6511804282302, places=4
+        )
 
 
 class dnpNMR_tester_sim(unittest.TestCase):
