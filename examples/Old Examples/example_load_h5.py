a2b9a7e00e80dd5f0481fe342abb7bd2f5b7ebc7
Thorsten Maly <tmaly@bridge12.com>
2021-04-01 08:30:08 -0400

    Added old examples
    Just to keep thing together.
    Slowly move them the examples folder and delete them.

---
+++ examples/Old Examples/example_load_h5.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+
+sys.path.append("..")
+import dnplab
+
+filename = "..data/h5/test.h5"
+
+ws = dnplab.dnpImport.h5.loadh5(filename)
+
+dnplab.dnpResults.plot(ws["ft"])
+dnplab.dnpResults.xlim(20, -20)
+dnplab.dnpResults.figure()
+dnplab.dnpResults.plot(ws["proc"], "o")
+
+dnplab.dnpResults.figure()
+ws["ft"].reorder(["power"])
+dnplab.dnpResults.imshow(ws["ft"].abs)
+
+dnplab.dnpResults.show()
