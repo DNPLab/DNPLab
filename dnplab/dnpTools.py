f236eb3b054bbbb900a73e5b8e272a4302db68a9
Yen-Chun Huang <yhuang@bridge12.com>
2021-09-23 15:25:48 -0400

    Updating Voigtian and Gaussian Normalization

--- dnplab/dnpTools.py
+++ dnplab/dnpTools.py
@@ -622,13 +622,15 @@ def voigtian(x, x0, sigma, gamma):
     Voigt is a combintaion of Gaussian and Lorentzian lineshapes
     """
     z = ((x0 - x) + 1j * gamma) / (sigma * np.sqrt(2.0))
-    fit = np.real(wofz(z))
+    fit = np.real(wofz(z)) / (sigma * np.sqrt(2 * np.pi))
     return fit
 
 
 def gaussian(x, x0, sigma):
-    return np.exp(-((x - x0) ** 2) / (2 * sigma ** 2))
+    return (
+        1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-((x - x0) ** 2) / (2 * sigma ** 2))
+    )
 
 
 def lorentzian(x, x0, gamma):
-    return (1./(np.pi*gamma))*gamma ** 2 / ((x - x0) ** 2 + gamma ** 2)
+    return (1.0 / (np.pi * gamma)) * gamma ** 2 / ((x - x0) ** 2 + gamma ** 2)
