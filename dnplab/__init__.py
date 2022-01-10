316dfb0294af4acff3b604116b09ae8daf10fbe6
Timothy Keller <tkeller@bridge12.com>
2022-01-10 17:18:05 -0500

    Updating analysis subpackage

--- dnplab/__init__.py
+++ dnplab/__init__.py
@@ -1,14 +1,20 @@
-from .dnpData import dnpdata, dnpdata_collection, create_workspace, return_data
+#from .dnpData import dnpdata, dnpdata_collection, create_workspace, return_data
 from .dnpImport import load
 from .dnpSave import save
-from .dnpNMR import *
+
+from . import analysis
+from . import constants
+#from . import core
+from . import fitting
 from . import io
 from . import math
-from .dnpFit import *
-from .dnpTools import *
-from .dnpMath import *
-from .dnpHydration import *
-from . import core
-from .dnpResults import *
+from . import processing
+from . import widgets
+#from .dnpFit import *
+#from .dnpTools import *
+#from .dnpMath import *
+#from .dnpHydration import *
+#from . import core
+#from .dnpResults import *
 #from .dnpWidgets import manualphase, manual_align
 from .version import __version__
