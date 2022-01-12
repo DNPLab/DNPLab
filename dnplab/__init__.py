a9c420d0e46e8a143e101c87edc7e30469dc2506
Timothy Keller <tkeller@bridge12.com>
2022-01-12 12:20:37 -0500

    testing constants subpackage

--- dnplab/__init__.py
+++ dnplab/__init__.py
@@ -1,12 +1,13 @@
 #from .dnpData import dnpdata, dnpdata_collection, create_workspace, return_data
-from .dnpImport import load
-from .dnpSave import save
+#from .dnpImport import load
+#from .dnpSave import save
 
-from . import analysis
+#from . import analysis
 from . import constants
+#from .constants import constants
 #from . import core
 from . import fitting
-from . import io
+#from . import io
 from . import math
 from . import processing
 from . import widgets
