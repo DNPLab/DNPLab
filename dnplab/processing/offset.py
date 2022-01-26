ce7a3b7316060476d9b772844c54b2d584afc19d
Timothy Keller <tkeller@bridge12.com>
2022-01-26 13:52:07 -0500

    Fix background fuction
    Adding remove_background function

--- dnplab/processing/offset.py
+++ dnplab/processing/offset.py
@@ -37,6 +37,23 @@ def remove_offset(data, dim="t2", offset_points=10):
 
     return data
 
+def remove_background(data, dim = "t2", deg = 0, regions = None):
+    """Remove polynomial background from data
+    """
+
+    proc_parameters = {
+        "dim": dim,
+        "deg": deg,
+        "regions": regions,
+    }
+
+    fit = background(data, dim = dim, deg = deg, regions = regions)
+    data = data - fit
+
+    proc_attr_name = "remove_backround"
+    data.add_proc_attrs(proc_attr_name, proc_parameters)
+
+    return data
 
 def background(data, dim = "t2", deg = 0, regions = None):
     """ Remove background from data
@@ -46,10 +63,13 @@ def background(data, dim = "t2", deg = 0, regions = None):
     out.unfold(dim)
 
     coord = out.coords[dim]
-    fit_points = [False for x in coord]
 
-    for region in regions:
-        fit_points = [fit_points[ix] or ((coord[ix] >= region[0]) & (coord[ix] <= region[1])) for ix in range(len(coord))]
+    if regions == None:
+        fit_points = [True for x in coord]
+    else:
+        fit_points = [False for x in coord]
+        for region in regions:
+            fit_points = [fit_points[ix] or ((coord[ix] >= region[0]) & (coord[ix] <= region[1])) for ix in range(len(coord))]
 
 
     for ix in range(out.shape[1]):
