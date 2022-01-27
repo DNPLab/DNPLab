a4898f3b7abcd4da7dbf8930b28bcce574d5c4bf
Timothy Keller <tkeller@bridge12.com>
2022-01-27 15:12:31 -0500

    All unittests pass

--- unittests/test_dnpTools.py
+++ unittests/test_dnpTools.py
@@ -16,25 +16,26 @@ class dnpTools_tester(unittest.TestCase):
 
         dnp.signal_to_noise(
             self.data,
+            dim = 't2',
         )
 
     def test_integrate(self):
-        dnp.integrate(self.data, dim = "f2")
+        dnp.integrate(self.data, dim = "t2")
 
     def test_mr_properties(self):
 
-        info_1H = dnp.constants.mr_properties("1H")
+        info_1H = dnp.mr_properties("1H")
         self.assertEqual(info_1H, 26.7522128)
 
-        info_1H = dnp.constants.mr_properties("1H", 0.35)
+        info_1H = dnp.mr_properties("1H", 0.35)
         self.assertEqual(info_1H, 14902114.17018196)
 
-        info_2H = dnp.constants.mr_properties("2H", "qmom")
+        info_2H = dnp.mr_properties("2H", "qmom")
         self.assertEqual(info_2H, 0.286)
 
-        info_6Li = dnp.constants.mr_properties("6Li", "natAbundance")
+        info_6Li = dnp.mr_properties("6Li", "natAbundance")
         self.assertEqual(info_6Li, 7.59)
 
-        info_6Li = dnp.constants.mr_properties("6Li", "relSensitivity")
+        info_6Li = dnp.mr_properties("6Li", "relSensitivity")
         self.assertEqual(info_6Li, 0.000645)
 
