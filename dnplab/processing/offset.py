62562df690b8cb14d73b3fad5d866630ece8d608
Timothy Keller <tkeller@bridge12.com>
2022-01-27 17:00:23 -0500

    Deleting remove_offset function

--- dnplab/processing/offset.py
+++ dnplab/processing/offset.py
@@ -1,42 +1,5 @@
 import numpy as np
 
-def remove_offset(data, dim="t2", offset_points=10):
-    """Remove DC offset from FID by averaging the last few data points and subtracting the average
-
-    Args:
-        all_data (dnpdata, dict): Data container for data
-
-    +---------------+------+---------+----------------------------------------------------------+
-    | parameter     | type | default | description                                              |
-    +===============+======+=========+==========================================================+
-    | dim           | str  | 't2'    | Dimension to calculate DC offset                         |
-    +---------------+------+---------+----------------------------------------------------------+
-    | offset_points | int  | 10      | Number of points at end of data to average for DC offset |
-    +---------------+------+---------+----------------------------------------------------------+
-
-    Returns:
-        dnpdata: data object with offset removed
-    """
-
-    proc_parameters = {
-        "dim": dim,
-        "offset_points": offset_points,
-    }
-
-    dim = proc_parameters["dim"]
-    offset_points = int(proc_parameters["offset_points"])
-
-    offsetData = data[dim, -1 * offset_points :].values
-    offsetData = offsetData.reshape(-1)
-    offset = np.mean(offsetData)
-
-    data -= offset
-
-    proc_attr_name = "remove_offset"
-    data.add_proc_attrs(proc_attr_name, proc_parameters)
-
-    return data
-
 def remove_background(data, dim = "t2", deg = 0, regions = None):
     """Remove polynomial background from data
     """
