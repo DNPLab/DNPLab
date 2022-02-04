d51d6c7be9b316f7ee514357e7c8b1b011e5bb3c
Timothy Keller <tkeller@bridge12.com>
2022-02-04 14:14:06 -0500

    Updating manual_phase widget

--- dnplab/widgets/manual_phase.py
+++ dnplab/widgets/manual_phase.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
 
-def manual_phase(data):
+def manual_phase(data, dim = "f2", fast = True):
     """Manually Phase NMR Spectra"""
 
     fig, ax = plt.subplots()
@@ -12,9 +12,16 @@ def manual_phase(data):
     plt.title("Manual Phase")
     plt.subplots_adjust(bottom=0.25)
     init_phase = 0
-    delta_phase = 0.1
-    (l0,) = plt.plot(data.coords["f2"], np.real(data.values), alpha=0.5)
-    (l,) = plt.plot(data.coords["f2"], np.real(data.values))
+    delta_phase = 1.0
+    coord = data.coords[dim]
+    size = coord.size
+    values = data.values
+    values = values.reshape(size, -1)
+    if fast:
+        values = values[:,0].reshape(-1,1)
+
+    l0 = plt.plot(coord, values, alpha=0.2)
+    l = plt.plot(coord, values)
     ax.margins(x=0)
 
     axcolor = "lightgoldenrodyellow"
@@ -26,21 +33,43 @@ def manual_phase(data):
 
     def update(val):
         phase = sphase.val
-        y_data = np.real(np.exp(-1j * np.pi * phase / 180.0) * data.values)
-        margin = 0.1 * (np.max(y_data) - np.min(y_data))
-        l.set_ydata(y_data)
-        ax.set_ylim(np.min(y_data) - margin, np.max(y_data) + margin)
+        margin = 0
+        min_y_data = 0
+        max_y_data = 0
+        for ix,line in enumerate(l):
+            y_data = np.real(np.exp(-1j * np.pi * phase / 180.0) * values[:,ix])
+            margin = max(0.1 * (np.max(y_data) - np.min(y_data)), margin)
+            line.set_ydata(y_data)
+            min_y_data = min(min_y_data, np.min(y_data))
+            max_y_data = max(max_y_data, np.max(y_data))
+        ax.set_ylim(min_y_data - margin, max_y_data + margin)
         fig.canvas.draw_idle()
 
     sphase.on_changed(update)
 
     resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-    button = Button(resetax, "Reset", color=axcolor, hovercolor="0.975")
+    reset_button = Button(resetax, "Reset", color=axcolor, hovercolor="0.975")
+
+    plus_90 = plt.axes([0.6, 0.025, 0.1, 0.04])
+    plus_90_button = Button(plus_90, "+90", color=axcolor, hovercolor="0.975")
+
+    minus_90 = plt.axes([0.4, 0.025, 0.1, 0.04])
+    minus_90_button = Button(minus_90, "-90", color=axcolor, hovercolor="0.975")
 
     def reset(event):
         sphase.reset()
 
-    button.on_clicked(reset)
+    def plus_90_phase(event):
+        new_phase = ((sphase.val + 270) % 360) - 180
+        sphase.set_val(new_phase)
+
+    def minus_90_phase(event):
+        new_phase = ((sphase.val + 90) % 360) - 180
+        sphase.set_val(new_phase)
+
+    reset_button.on_clicked(reset)
+    plus_90_button.on_clicked(plus_90_phase)
+    minus_90_button.on_clicked(minus_90_phase)
 
     plt.show()
     manual_phase = sphase.val
