c26c7cf624797e085428f83f2bfafd521eb1d82a
Thorsten Maly <tmaly@bridge12.com>
2022-02-25 16:14:32 -0500

    adding autosummary_generate variable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 import os
 import sys
 
+
 sys.path.insert(0, os.path.abspath("../../"))
 print("path:")
 print(sys.path)
@@ -90,6 +91,8 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_gallery.gen_gallery",
 ]
+
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
