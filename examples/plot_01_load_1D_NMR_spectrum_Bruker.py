5bbcb5832693873d241a643ece6342c3162e7904
Thorsten Maly <tmaly@bridge12.com>
2022-02-26 17:34:42 -0500

    Updated NMR example

--- examples/plot_01_load_1D_NMR_spectrum_Bruker.py
+++ examples/plot_01_load_1D_NMR_spectrum_Bruker.py
@@ -18,25 +18,23 @@ import matplotlib.pylab as plt
 # %%
 # Load and Process the NMR Spectrum
 # ---------------------------------
-# First use some sample 1D TopSpin data. Example data is located in the data folder. All data enters into the same object structure so this example applies to any NMR format.
+# In this example we use a 1D NMR spectrum aqcuired using TopSpin. Example data is located in the data folder. All data enters into the same object structure so this example applies to any NMR format. DNPLab will try to identify the format of the NMR spectrum (e.g. TopSpin, VnmrJ, Kea, etc.). This will work in most cases. If the autodetect fails, the format can be explicitely given using the data_type attribute of the load function (e.g. data_type = "topspin").
 
 data = dnp.load("../data/topspin/1")
 
 # %%
-# Raw data are now in both ws["proc"] and ws["raw"]. The following processing steps will only modify ws["proc"] but leaves ws["raw"] untouched. That way, you can always return to the original data. Furthermore, when you save the workspace (ws), you will always safe a copy of the raw data.
-#
-# Next, perform some basic processing. Simply pass the entire workspace, 'ws', to the functions and ws["proc"] will be manipulated. For example:
+# The spectral data is now stored in the data object. DNPLab uses object oriented programming, therefore, data is not just a variable. It is a dnpData object. Next, we will perform some basic processing, methods that are pretty standard in NMR spectroscopy. For this just pass the dnpData object from one function to the next.
 
-#data = dnp.remove_background(data)
+# data = dnp.remove_background(data)
 data = dnp.apodize(data, lw = 100)
 data = dnp.fourier_transform(data)
 
 # %%
-# This procedure removes the DC offset, performs a apodization with a window function (100 Hz linewidth), followed by a Fourier transforms.
+# This procedure removes the DC offset, performs an apodization with a window function (100 Hz linewidth), followed by a Fourier transforms. By default a zero filling of factor two is performed prioer to Fourier transforming the spectrum.
 #
-# NMR spectra are typically phase sensitive and the final processing step is often phasing the spectra. In DNPLab you can either manually phase correct the spectrum using the 'autophase' function using the 'manual' keyword and giving a phase explicitely:
+# NMR spectra are typically phase sensitive (complex data) and the final processing step is to phase the spectrum. In DNPLab you can either manually phase correct the spectrum using the 'autophase' function using the 'manual' keyword and by giving a phase explicitely:
 
-#data = dnp.autophase(data, method = "manual", phase = -1*np.pi / 2)
+data = dnp.autophase(data, method = "manual", phase = -1*dnp.constants.pi / 2)
 
 # %%
 # Or by using the autophase function to automatically search for and apply the best phase angle
@@ -46,30 +44,36 @@ data = dnp.autophase(data, force_positive = True)
 # %%
 # Plot Processed Data
 # -------------------
+
+# %%
+# DNPLab automatically takes care of the name of the coordinates. For NMR spectra, the dimensions are named "t2" and "t1" for the direct and indirect dimension of a FID, respectively. After the Fourier Transformation these dimensions will be renamed to "f2" and "f1", respectively. Dimensions can be renamed by the user. However, several features in DNPLab take advantage of the dimension name and processing or plotting functions will change their behavior according to the type of the spectrum.
+
+# %%
+# To plot the NMR spectrum simply use the following command. By default, only the real part of the NMR spectrum is shown.
+
+dnp.dnplabplot(data)
+
+# %%
+# Note, that the DNPLab plotting function will automatically reverse the direction of the x axis, as it is custom for NRM spectra and axis labels are automatically generated
+
+# %%
+# The integrated plotting function allows for additional input arguments. For example to only show a portion of the spectrum use the xlim argument. A title can be passed to the function using the title argument. This can also be a variable. For example:
+
+sampleTag = 'ODNP Experiment of 10 mM TEMPO in Water'
+
+dnp.dnplabplot(data, xlim = [-100, 100], title = sampleTag)
+
+# %%
+# Advanced Usage
+# --------------
+# The dnpData objects stores many more attributes for the data set, which are loaded automatically when loading the experimental data. For example, the NMR frequency can be easily accessed as follows:
+
+nmr_frequency = data.attrs["nmr_frequency"]
+print(nmr_frequency)
+
+# %%
+# All variables in DNPLab are stored in SI units. Therefore, the NMR frequency is given in (Hz).
 # If needed, access your processed spectrum as follows:
 
 x_axis = data.coords["f2"]        # ppm axis
 spectrum = data.values            # spectrum
-
-# %%
-# DNPLab automatically takes care of the name of the coordinates. By default, the dimensions are named "t2" for the direct dimensions and "t1" for the indirect dimension before performing a Fourier Transformation. After the Fourier Transformation these dimensions will be called "f2" and "f1". However, you can rename dimensions at anytime. For example, like this:
-
-
-# %%
-# Access data attributes such as the NMR frequency as follows:
-
-nmr_frequency = data.attrs["nmr_frequency"]
-
-# %%
-# The following will generate a plot of the NMR spectrum, and will show the imaginary and real part in the same figure.
-
-
-
-plt.figure()
-dnp.plot(data)
-plt.xlabel('Chemical Shift (ppm)')
-plt.ylabel('Signal (a.u.)')
-plt.xlim(-100,100)
-plt.grid()
-plt.show()
-
