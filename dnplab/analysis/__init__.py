08a967bede1ec8a0943d21a9a0e9bb8c9a1f1ac1
Thorsten Maly <tmaly@bridge12.com>
2022-04-19 08:15:25 -0400

    Adding relaxation_fit module

--- dnplab/analysis/__init__.py
+++ dnplab/analysis/__init__.py
@@ -1,2 +1,3 @@
 """Modules which provide a workflow for processing dnpData objects"""
 from . import hydration
+from . import relaxation_fit
---
+++ dnplab/analysis/relaxation_fit.py
@@ -0,0 +1,5 @@
+import numpy as np
+# from scipy import interpolate
+# from scipy import optimize
+
+
