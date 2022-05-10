3acdbe750e2ac5426c2b4457053e1a61eb273403
Timothy Keller <tkeller@bridge12.com>
2022-05-10 18:30:52 -0400

    WIP Testing Gaussian window function

--- dnplab/math/window.py
+++ dnplab/math/window.py
@@ -47,10 +47,12 @@ def gaussian(x, lw):
     Returns:
         array: gaussian window function
     """
-    sigma = lw / (
+    sigma = lw * (
         2.0 * np.sqrt(2.0 * np.log(2.0))
+
     )  # convert FWHM to standard deviation
-    return np.exp(-1 * (x**2.0) / (2.0 * sigma**2.0))
+#    sigma = 1. / sigma
+    return np.exp(-1 * (x**2.0) * (sigma**2.0)/1.5)
 
 
 def hann(x):
