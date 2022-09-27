dcf915afa42173302fb3cc9f495eeb1d3ebdcd7d
Thorsten Maly <tmaly@bridge12.com>
2022-09-27 12:27:24 -0400

    Fixed T1 fitting example

--- examples/02_Analysis/plot_02_analyze_inversion_recovery_experiments.py
+++ examples/02_Analysis/plot_02_analyze_inversion_recovery_experiments.py
@@ -33,10 +33,10 @@ data.attrs["experiment_type"] = "nmr_spectrum"
 data = dnp.remove_background(data)
 
 # dnp.remove_background(data)
-# data = dnp.apodize(data, lw=100)
-# data = dnp.fourier_transform(data)
+data = dnp.apodize(data, lw=100)
+data = dnp.fourier_transform(data)
 
-# data = dnp.autophase(data, method="manual", phase=90)
+data = dnp.autophase(data, method="manual", phase=90)
 
 # dnp.fancy_plot(data, xlim=[-50, 80], title="Inversion Recovery")
 # dnp.plt.show()
@@ -47,7 +47,7 @@ data = dnp.remove_background(data)
 # --------------------------------
 # The spectra shown here are the individual NMR spectra for different evolution times. Note how the spectra are not perfectly aligned? This can be fixed using the aligning function. A more detailed description of the aligning routine of DNPLab is given in the tutorial :ref:`plot_01_align_nmr_spectra`.
 
-# data = dnp.align(data)
+data = dnp.align(data)
 
 # dnp.fancy_plot(data, xlim=[-50, 80], title="Inversion Recovery, aligned")
 # dnp.plt.show()
@@ -58,44 +58,41 @@ data = dnp.remove_background(data)
 # -----------------
 # To determine the T1 relaxation time, we first have to integrate the peak intensity across the entire spectrum. After integration the workspace will have a new dnpdata object called "integrals" where the integral values and indirect axis are kept. To use ``fancy_plot`` the attribute experiment_type has to be changed to "inversion_recovery".
 
-
-# dnp.plt.figure()
-# dnp.waterfall(data, dx = 50, dy = 100000)
-# dnp.plt.plot(data.values[:,7].real)
-# dnp.plt.show()
-# #
-
-
-# dnp.plt.figure()
-# dnp.plt.plot(data.values[:, 1].real)
-# dnp.plt.show()
-
-# integrals = dnp.integrate(data)
-
-# print(integrals.values[:,0].real )
-# print(integrals)
-
-
-# integrals.attrs["experiment_type"] = "inversion_recovery"
+integrals = dnp.integrate(data)
 
 # dnp.fancy_plot(integrals)
 # dnp.plt.show()
 
-# initial_guess = (2., -4000, 4000) # initial guess for: T1, M_0, M_inf
 
-# out = dnp.fit(dnp.math.relaxation.t1, integrals, dim = 't1', p0 = initial_guess)
+# Depending on the quality of the data it is sometimes better to not integrate over the entire spectrum but to pick only integrate over the peak region. Here, to integrate over a region of 0 to 20 ppm us the following command:
 
-# fit = out['fit']
-# popt = out['popt']
-# err = out['err']
+integrals = dnp.integrate(data, regions=[(0, 20)])
 
 # dnp.fancy_plot(integrals)
-# dnp.plot(fit, '-')
-# print(popt)
-# T1 = popt['popt',0]
-# M_0 = popt['popt',1]
-# M_inf = popt['popt',2]
-
-# print(T1.values)
-
 # dnp.plt.show()
+
+integrals.attrs["experiment_type"] = "inversion_recovery"
+
+# %%
+# Fit Data
+# --------
+# To get the T1 value an inversion recovery function is fitted to the data sets. The fit requires an initial guess.
+
+initial_guess = (2.0, -4000, 4000)                                                  # initial guess for: T1, M_0, M_inf
+out = dnp.fit(dnp.math.relaxation.t1, integrals, dim = 't1', p0 = initial_guess)
+
+fit = out['fit']
+popt = out['popt']
+err = out['err']
+
+dnp.fancy_plot(integrals, title="Inversion Recovery")
+dnp.plot(fit, '-')
+dnp.plt.show()
+
+T1 = popt['popt',0]
+M_0 = popt['popt',1]
+M_inf = popt['popt',2]
+
+print(T1.values)
+print(M_0.values)
+print(M_inf.values)
