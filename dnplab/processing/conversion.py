fe2b2decce6a0578e56a8a462dee17eb45205001
Thorsten Maly <tmaly@bridge12.com>
2022-09-30 12:56:57 -0400

    Running black

--- dnplab/processing/conversion.py
+++ dnplab/processing/conversion.py
@@ -1,5 +1,6 @@
 import numpy as _np
 
+
 def dBm2w(power_in_dBm):
     """Convert power in dBm to power in W
 
@@ -10,9 +11,10 @@ def dBm2w(power_in_dBm):
 
     """
 
-    power_in_W =  10.0**(power_in_dBm/10.0)/1000.0
+    power_in_W = 10.0 ** (power_in_dBm / 10.0) / 1000.0
 
     return power_in_W
+
 
 def w2dBm(power_in_W):
     """Convert power in W to power in dbM
@@ -24,9 +26,6 @@ def w2dBm(power_in_W):
 
     """
 
-    power_in_dBm =  10.0*_np.log10(1000 * power_in_W)
+    power_in_dBm = 10.0 * _np.log10(1000 * power_in_W)
 
     return power_in_dBm
-
-
-
