2388eb87a36b1d3bf80f1e8d61a6749d0415d52a
Thorsten Maly <tmaly@bridge12.com>
2022-10-16 15:13:26 -0400

    Corrected typos

--- dnplab/processing/conversion.py
+++ dnplab/processing/conversion.py
@@ -13,12 +13,13 @@ def dBm2w(power_in_dBm):
         float (array): Power in (W)
 
     """
+    print('This is dBm2w')
 
     power_in_W = 10.0 ** (power_in_dBm / 10.0) / 1000.0
 
     # Set values below -199 dBm to 0 W
-    index = np.were(power_in_dBm <= -199)
-    power_in_W[index] = 0
+    index = _np.where(power_in_dBm <= -199)
+    power_in_W[index] = 0.0
 
     return power_in_W
 
