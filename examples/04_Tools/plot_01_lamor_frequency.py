32c5badcbb626ba094ebd881a1e042c819e80067
unknown <krieger@bridge12.com>
2023-03-14 16:42:23 -0400

    black reformatting, to suceed in build tests

--- examples/04_Tools/plot_01_lamor_frequency.py
+++ examples/04_Tools/plot_01_lamor_frequency.py
@@ -19,9 +19,9 @@ This example demonstrates how to use the tool to calculate the nuclear Larmor fr
 # * Nuclear Quadrupole Moment (fm^2, 100 barns)
 # * Isotope Natural Abundance (%)
 # * Relative Sensitivity with Respect to 1H at same B_{0}
-# 
+#
 # (for nuclei with I > 1/2), and some more parameters. This dictionary can be used to provide nuclear properties in any calculation, it is also used by the DNPLab function ``mr_properties``.
-# 
+#
 # To get started, first, setup the python environment:
 
 import dnplab as dnp
@@ -34,17 +34,17 @@ import matplotlib.pyplot as plt
 # %%
 # **Proton Gyromagnetic Ratio**
 
-print("v_L(1H) = ", dnp.mr_properties('1H'), "(Hz/T)")
+print("v_L(1H) = ", dnp.mr_properties("1H"), "(Hz/T)")
 
 # %%
 # **Nirogen 14N nuclear spin quantum number**
 
-print("I = ", dnp.mr_properties('14N','spin'))
+print("I = ", dnp.mr_properties("14N", "spin"))
 
 # %%
 # **Carbon 13 (13C) natural abundance**
 
-print(dnp.mr_properties('13C','natAbundance'), " %")
+print(dnp.mr_properties("13C", "natAbundance"), " %")
 
 
 spinQuatnumNumber = np.empty(121)
@@ -55,9 +55,3 @@ for each in dnp.gmrProperties.keys():
     spinQuatnumNumber[k] = dnp.gmrProperties[each][0]
 
     k = k + 1
-
-
-
-
-
-
