57482415a4e0914ed9d1993acd9ca1036714c18e
Timothy Keller <tkeller@bridge12.com>
2023-03-27 11:56:55 -0400

    Updated tnmr import module

--- dnplab/io/tnmr.py
+++ dnplab/io/tnmr.py
@@ -4,11 +4,12 @@ import struct
 import re
 
 
-def import_tnmr(path):
+def import_tnmr(path, squeeze=True):
     """Import tnmr data and return DNPData object
 
     Args:
         path (str) : Path to .jdf file
+        squeeze (bool) : automatically remove length 1 dimensions
 
     Returns:
         tnmr_data (object) : DNPData object containing tnmr data
@@ -18,6 +19,9 @@ def import_tnmr(path):
     values, dims, coords = import_tnmr_data(path)
 
     tnmr_data = DNPData(values, dims, coords, attrs)
+
+    if squeeze:
+        tnmr_data.squeeze()
 
     return tnmr_data
 
@@ -56,51 +60,48 @@ def import_tnmr_data(path):
         raw = f.read()
 
     tmag_attrs = {}
-    tmag_attrs['version'] = str(raw[0:8])
+    tmag_attrs["version"] = str(raw[0:8])
 
-    len_tecmag_struct = int.from_bytes(raw[16:20], 'little')
-    tecmag_struct = raw[20:20+len_tecmag_struct]
+    len_tecmag_struct = int.from_bytes(raw[16:20], "little")
+    tecmag_struct = raw[20 : 20 + len_tecmag_struct]
 
-    offset = 28+len_tecmag_struct
-    len_data = raw[offset:offset+4]
+    offset = 28 + len_tecmag_struct
+    len_data = raw[offset : offset + 4]
     offset += 4
 
-    len_data = int.from_bytes(len_data, 'little')
+    len_data = int.from_bytes(len_data, "little")
 
-    data = raw[offset:offset+(len_data)]
+    data = raw[offset : offset + (len_data)]
 
-    data = _np.frombuffer(data, dtype = '<f')
+    data = _np.frombuffer(data, dtype="<f")
 
     data = data[0::2] + 1j * data[1::2]
 
     # Parse tecmag struct
 
-    # points in 1d, 2d, 3d, 4d
-    npts = _np.frombuffer(tecmag_struct[0:16], dtype = '<i')
+    # points in x0, x1, x2, x3
+    npts = _np.frombuffer(tecmag_struct[0:16], dtype="<i")
 
-    actual_npts = _np.frombuffer(tecmag_struct[16:32], dtype = '<i')
+    # Array of points in each dimension (x0, x1, x2, x3)
+    actual_npts = _np.frombuffer(tecmag_struct[16:32], dtype="<i")
 
-    acq_pts = int.from_bytes(tecmag_struct[32:36], byteorder = 'little')
-    scans = int.from_bytes(tecmag_struct[36:40], byteorder = 'little')
-    actual_scans = int.from_bytes(tecmag_struct[40:44], byteorder = 'little')
-    dummy_scans = int.from_bytes(tecmag_struct[44:48], byteorder = 'little')
+    acq_pts = int.from_bytes(tecmag_struct[32:36], byteorder="little")
+    scans = int.from_bytes(tecmag_struct[36:40], byteorder="little")
+    # actual_scans = int.from_bytes(tecmag_struct[40:44], byteorder = 'little')
+    # dummy_scans = int.from_bytes(tecmag_struct[44:48], byteorder = 'little')
 
-    sw = struct.unpack('<4d', tecmag_struct[240:272])
-    dwell_time = struct.unpack('<4d', tecmag_struct[272:304])
-    #dwell_time = float.from_bytes(tecmag_struct[272:304], byteorder = 'little')
+    sw = struct.unpack("<4d", tecmag_struct[240:272])
+    dwell_time = struct.unpack("<4d", tecmag_struct[272:304])
+    # dwell_time = float.from_bytes(tecmag_struct[272:304], byteorder = 'little')
 
-    print(actual_npts)
-    print(acq_pts)
-    print(sw)
-    print(dwell_time)
+    data = data.reshape(npts, order="F")
 
-    data = data.reshape(npts[::-1])
-    data = data.squeeze()
+    coords = []
 
+    for ix, pts in enumerate(npts):
+        coord = _np.r_[0:pts] * dwell_time[ix]
+        coords.append(coord)
 
-    print(data.shape)
-
-
-    time = _np.r_[0:npts[0]]*dwell_time[0]
+    dims = ["t2", "t1", "t3", "t4"]  # t2 dim is first
 
     return data, dims, coords
