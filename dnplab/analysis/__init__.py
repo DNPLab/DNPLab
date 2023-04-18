8b7ae17c581b29d5756f8d2f23401632329181c2
Thorsten Maly <tmaly@bridge12.com>
2023-04-18 16:35:05 -0700

    rename module

--- dnplab/analysis/__init__.py
+++ dnplab/analysis/__init__.py
@@ -1,4 +1,4 @@
 """Modules which provide a workflow for analyzing dnpData objects"""
 from . import hydration
 from . import relaxation_fit
-from . import enhancement_profiles
+from . import dnp_helpers
--- dnplab/io/specman.py
+++ dnplab/io/specman.py
@@ -119,20 +119,20 @@ def load_specman_d01(path, attrs, verbose=True):
         data = _np.reshape(data, (attrs["numberOfVariables"], uint_read[3]))
 
     elif attrs["dims"] == 2:
-        data = _np.reshape(data, (uint_read[0], uint_read[3], uint_read[4]))
+        data = _np.reshape(data, (uint_read[0], uint_read[4], uint_read[3]))
         # data = _np.reshape(data, (uint_read[2], uint_read[3], uint_read[4]))
         print("2D data")
 
     elif attrs["dims"] == 3:
         data = _np.reshape(
-            data, (uint_read[0], uint_read[3], uint_read[4], uint_read[5])
+            data, (uint_read[0], uint_read[5], uint_read[4], uint_read[3])
             # data, (uint_read[2], uint_read[3], uint_read[4], uint_read[5])
         )
         print("3D data")
 
     elif attrs["dims"] == 4:
         data = _np.reshape(
-            data, (uint_read[0], uint_read[3], uint_read[4], uint_read[5], uint_read[6])
+            data, (uint_read[0], uint_read[6], uint_read[5], uint_read[4], uint_read[3])
         )
         print("4D data")
 
