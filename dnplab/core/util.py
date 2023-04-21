befa19e4830104120bdb8ad0eda3fd1ae4becb6b
Thorsten Maly <tmaly@bridge12.com>
2023-04-21 15:35:04 -0700

    New function to update dnpdata object coords and dims

--- dnplab/core/util.py
+++ dnplab/core/util.py
@@ -54,3 +54,51 @@ def concat(data_list, dim, coord=None):
     data = DNPData(values, dims, coords, attrs)
 
     return data
+
+
+def update_axis(data, new_dims, start_stop, spacing="lin", verbose=False):
+    """Update axis
+
+    Update dimensions (dims) and axis (coords) of a dnpDate object. The name of the dims will be replaced with the name giving in new_dims. The variable start_stop defines the values of the new coords. This can be either a tuple (start values, stop value) or a vector with values. If the start and stop value is provided, either a linear axis (spacing = "lin", default) or a logarithmically space (spacing = "log") will be created. The new axis will replace the coords in the dnpdata object.
+
+    The function is currently implemented for 1D objects only.
+
+    Args:
+        data (DNPData): dnpData objetc
+        new_dims (str): Name of the new dimension. If None the name will not be changed.
+        start_stop(tuple or vector): Coords for new dimension
+        spacing (str): "lin" for linear spaced axis or "log" for logarithmically spaced axis
+
+    Returns:
+        data (DNPData): concatenated data object
+    """
+
+    out = data.copy()
+
+    if new_dims == None:
+        new_dims = out.dims[0]
+
+    out.rename(out.dims[0], new_dims)
+
+    if verbose == True:
+
+        print("New dims name:", new_dims)
+
+    if len(start_stop) == 2:
+
+        if spacing == "lin":
+
+            out.coords[new_dims] = _np.linspace(
+                start_stop[0], start_stop[1], len(out.values)
+            )
+
+        elif spacing == "log":
+            out.coords[new_dims] = _np.logspace(
+                start_stop[0], start_stop[1], len(out.values)
+            )
+
+    elif len(start_stop) > 2:
+
+        out.coords[new_dims] = start_stop
+
+    return out
