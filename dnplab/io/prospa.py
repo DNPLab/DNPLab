20d959d0a733b412cd0e6c07c9f94533efe26f97
Yen-Chun Huang <yhuang@bridge12.com>
2023-05-23 16:44:39 -0400

    Change some dnplab attributes in prospa format

--- dnplab/io/prospa.py
+++ dnplab/io/prospa.py
@@ -351,14 +351,13 @@ def prospa_attrs4dnplab(exp_attrs):
     """
     dnplab_attrs = {}
     dnplab_attrs["experiment_type"] = "nmr_spectrum"
-    experiment = exp_attrs["experiment"]
-    dnplab_attrs["prospa_program_name"] = experiment
+    dnplab_attrs["spectrometer_format"] = exp_attrs["spectrometer_format"]
     dnplab_attrs["spectrometer_frequency"] = exp_attrs["nmr_frequency"] # Hz
     dnplab_attrs["number_of_scans"] = exp_attrs["nrScans"]
     dnplab_attrs["90_pulse_length"] = exp_attrs["90Amplitude"]
     dnplab_attrs["receiver_gain"] = exp_attrs["rxGain"]
     try:
-        dnplab_attrs["power"] = exp_attrs["power"]
+        dnplab_attrs["power"] = exp_attrs["Power"]
     except:
         pass
     try:
@@ -366,11 +365,11 @@ def prospa_attrs4dnplab(exp_attrs):
     except:
         pass
 
-    if experiment == "T1-IR-FID" or experiment == "B12T_T1-IR-FID" or experiment == "B12T_T1-IR-FID_MPS":
+    if exp_attrs["experiment"] == "T1-IR-FID" or exp_attrs["experiment"] == "B12T_T1-IR-FID" or exp_attrs["experiment"] == "B12T_T1-IR-FID_MPS":
         dnplab_attrs["minimum_delay"] = exp_attrs["minDelay"] * 1e-3 # s
         dnplab_attrs["maximum_delay"] = exp_attrs["maxDelay"] * 1e-3 # s
     
-    if experiment == "B12T_jres2D":
+    if exp_attrs["experiment"] == "B12T_jres2D":
         dnplab_attrs["number_of_steps"] = exp_attrs["nrSteps"]
         dnplab_attrs["inter_pulse_delay"] = exp_attrs["interPulseDelay"]
         dnplab_attrs["increment"] = exp_attrs["increment"]
