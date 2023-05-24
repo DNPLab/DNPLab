04a0ec74cd631bcc77f4bed40af6ad34f68391ae
Yen-Chun Huang <yhuang@bridge12.com>
2023-05-24 09:54:33 -0400

    Some updates

--- unittests/test_exponential_window.py
+++ unittests/test_exponential_window.py
@@ -3,27 +3,29 @@ from numpy.testing import assert_array_equal
 import dnplab as dnp
 import numpy as np
 
-class expoinential_window_tester(unittest.TestCase):
+class exponential_window_tester(unittest.TestCase):
     def setUp(self):
         lw = 1
         x = np.r_[0:10:1024j]
         y = dnp.window.exponential(x, lw)
-        self.data = dnp.DNPData(y, ['t2'], [x])
+        y = y + 0j
         
-        dt = x[1] - x[0]
-        self.f = np.r_[-1/(2*dt):1/(2*dt):1024j]
-        self.lorizen_distribution = 1 / dnp.pi * (1 / ((self.f)**2 + 0.5*lw**2))
-        self.lorizen_distribution /= np.max(self.lorizen_distribution)
-        print(self.lorizen_distribution)
+        self.data = dnp.DNPData(y, ['t2'], [x])
+        self.data_ft = dnp.fourier_transform(self.data.copy(), convert_to_ppm=False, zero_fill_factor = 1)
+        self.data_ft /= np.max(self.data_ft)
+        self.f = self.data_ft.coords['f2']
+        self.lorentzian_distribution = 1 / dnp.pi * (1 / ((self.f)**2 + 0.5*lw**2))
+        self.lorentzian_distribution /= np.max(self.lorentzian_distribution)
         
     def test_exponential_window(self):
-        self.data = dnp.fourier_transform(self.data, convert_to_ppm=False)
-        self.data /= np.max(self.data)
-        print(self.data.values)
         dnp.plt.figure()
-        dnp.plot(self.data)
-        dnp.plt.plot(self.f, self.lorizen_distribution)
+        dnp.plot(self.data, label = 'window')
+
+        dnp.plt.figure()
+        dnp.plot(self.data_ft, label = 'FFT window')
+        dnp.plt.plot(self.f, self.lorentzian_distribution, label = 'Lorentzian')
+        dnp.plt.legend()
         dnp.plt.show()
 
 if __name__ == "__main__":
-    pass
+    unittest.main()
