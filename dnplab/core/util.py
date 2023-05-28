26ae78a7a14da1b5825d6c9a63769820482a43f0
Thorsten Maly <tmaly@bridge12.com>
2023-05-28 11:46:51 +0200

    Update to function
    Now the dimension can be selected.

--- dnplab/core/util.py
+++ dnplab/core/util.py
@@ -56,7 +56,7 @@ def concat(data_list, dim, coord=None):
     return data
 
 
-def update_axis(data, old_dims= 0, new_dims, start_stop, spacing="lin", verbose=False):
+def update_axis(data, start_stop, dim=0, new_dims=0, spacing="lin", verbose=False):
     """Update axis
 
     Update dimensions (dims) and axis (coords) of a dnpDate object. The name of the dims will be replaced with the name giving in new_dims. The variable start_stop defines the values of the new coords. This can be either a tuple (start values, stop value) or a vector with values. If the start and stop value is provided, either a linear axis (spacing = "lin", default) or a logarithmically space (spacing = "log") will be created. The new axis will replace the coords in the dnpdata object.
@@ -64,21 +64,23 @@ def update_axis(data, old_dims= 0, new_dims, start_stop, spacing="lin", verbose=
     The function is currently implemented for 1D objects only.
 
     Args:
-        data (DNPData): dnpData object
-        new_dims (str): Name of the new dimension. If None the name will not be changed.
-        start_stop(tuple or vector): Coords for new dimension
-        spacing (str): "lin" for linear spaced axis or "log" for logarithmically spaced axis
+        data (DNPData):                 dnpData object
+        start_stop(tuple or vector):    Coords for new dimension
+        dim (int):                      Dimension to act on
+        new_dims (str):                 Name of the new dimension. If None the name will not be changed.
+        spacing (str):                  "lin" for linear spaced axis or "log" for logarithmically spaced axis
 
     Returns:
         data (DNPData): concatenated data object
     """
 
     out = data.copy()
+    data_shape = _np.shape(out.values)
 
     if new_dims == None:
-        new_dims = out.dims[old_dims]
+        new_dims = out.dims[dim]
 
-    out.rename(out.dims[old_dims], new_dims)
+    out.rename(out.dims[dim], new_dims)
 
     if verbose == True:
         print("New dims name:", new_dims)
@@ -86,12 +88,12 @@ def update_axis(data, old_dims= 0, new_dims, start_stop, spacing="lin", verbose=
     if len(start_stop) == 2:
         if spacing == "lin":
             out.coords[new_dims] = _np.linspace(
-                start_stop[0], start_stop[1], len(out.values)
+                start_stop[0], start_stop[1], data_shape[dim]
             )
 
         elif spacing == "log":
             out.coords[new_dims] = _np.logspace(
-                start_stop[0], start_stop[1], len(out.values)
+                start_stop[0], start_stop[1], data_shape[dim]
             )
 
     elif len(start_stop) > 2:
