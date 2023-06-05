b8a49c83cc2f9047a78a3a2b8e840d2e8d3b730e
Timothy Keller <tkeller@bridge12.com>
2023-06-05 11:37:45 -0400

    Updated Error Message for Apodization

--- dnplab/processing/apodization.py
+++ dnplab/processing/apodization.py
@@ -65,9 +65,13 @@ def apodize(data, dim="t2", kind="exponential", **kwargs):
 
     data = data.copy()
 
-    coord = data.coords[dim]
     index = data.index(dim)
+    coord = data.coords[dim]
 
+    kind = str(kind).lower() # kind of apodization is a lower case string
+
+    if kind not in _windows:
+        raise ValueError('Window function "%s" not valid. Available window functions are: %s.  See documentation for more details.'%(kind,list(_windows.keys())))
     window = _windows[kind]
     apwin = window(coord, **kwargs)
 
