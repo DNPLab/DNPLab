5cf999c8678036e268746218ef993f255085f7ec
Yen-Chun Huang <yhuang@bridge12.com>
2023-06-08 16:49:00 -0400

    Add solution to average proc_attrs issue

--- dnplab/processing/average.py
+++ dnplab/processing/average.py
@@ -17,7 +17,12 @@ def average(data, axis="Average"):
     """
 
     data = data.copy()
+    proc_attr_name = "average"
+    proc_parameters = {"axis": axis}
+    proc_attrs_list = data.proc_attrs
 
     data = _np.mean(data, axis=axis)  # it will automatically assign proc_attrs
+    data.proc_attrs = proc_attrs_list
+    data.add_proc_attrs(proc_attr_name, proc_parameters)
 
     return data
