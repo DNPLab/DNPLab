f5c331fc08d0646c617f918a2e717ac2eed46836
Thorsten Maly <tmaly@bridge12.com>
2023-06-12 17:06:50 -0400

    Fixed unittest

--- unittests/test_io.py
+++ unittests/test_io.py
@@ -11,18 +11,18 @@ class import_topspin_tester(unittest.TestCase):
     def test_import_topspin_exp1_is_fid(self):
         data = dnp.load(os.path.join(self.testdata, str(1)), data_format="topspin")
         self.assertEqual(data.dims[0], "t2")
-        self.assertEqual(data.values.size, 8148)
+        self.assertEqual(data.values.size, 8192)
         self.assertAlmostEqual(data.attrs["nmr_frequency"], 14831413.270000001)
 
     def test_import_topspin_exp5_is_2d_phcyc(self):
         data = dnp.load(os.path.join(self.testdata, str(5)), data_format="topspin")
-        self.assertEqual(data.values.shape[0], 11913)
+        self.assertEqual(data.values.shape[0], 11973)
         self.assertEqual(data.dims, ["t2", "t1"])
         self.assertAlmostEqual(data.attrs["nmr_frequency"], 14831413.270000001)
 
     def test_import_topspin_exp28_is_2d(self):
         data = dnp.load(os.path.join(self.testdata, str(28)), data_format="topspin")
-        self.assertEqual(data.values.shape, (7923, 8))
+        self.assertEqual(data.values.shape, (7983, 8))
         self.assertEqual(data.dims, ["t2", "t1"])
         self.assertAlmostEqual(data.attrs["nmr_frequency"], 14831413.270000001)
 
--- unittests/test_load.py
+++ unittests/test_load.py
@@ -29,7 +29,7 @@ class load_wrapper_tester(unittest.TestCase):
     def test_topspin(self):
         data = dnp.load(os.path.join(self.topspin_dir, str(1)), data_format="topspin")
         self.assertEqual(data.dims[0], "t2")
-        self.assertEqual(data.values.size, 8148)
+        self.assertEqual(data.values.size, 8192)
         self.assertAlmostEqual(data.attrs["nmr_frequency"], 14831413.270000001)
 
     def test_prospa(self):
