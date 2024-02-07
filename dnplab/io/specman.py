a8a877ed18894cc7cb7755046205b617cc21c0e5
Yen-Chun Huang <yhuang@bridge12.com>
2024-02-07 10:26:13 -0500

    Add a function to calculate specman coords based on the attrs

--- dnplab/io/specman.py
+++ dnplab/io/specman.py
@@ -1,7 +1,7 @@
 import numpy as _np
 import os
 import dnplab as _dnp
-
+import re
 
 def import_specman(path):
     """Import SpecMan data and return DNPData object
@@ -76,7 +76,8 @@ def load_specman_exp(path):
             attrs[c + "_" + str(i)] = splt_exp_content
         else:
             pass
-
+    
+    attrs = analyze_attrs(attrs)
     return attrs
 
 
@@ -157,11 +158,74 @@ def load_specman_d01(path, attrs, verbose=False):
     dims_full = ["x0", "x1", "x2", "x3", "x4"]
     dims = dims_full[0 : dataShape[0] + 1]
 
-    coords = []
-
-    shape = _np.shape(data)
-
-    for index in range(data.ndim):
-        coords.append(_np.arange(0.0, shape[index]))
-
+    dims, coords = specman_coords(attrs, data, dims)
+    
     return data, dims, coords, attrs
+
+def analyze_attrs(attrs):
+    """
+    Analyze the attrs and add some important attrs to existing dictionary
+
+    Args:
+        attrs (dict): Dictionary of specman acqusition parameters
+    
+    Returns:
+        attrs (dict): The dictionary of specman acqusition parameters and added parameters
+
+    """
+    
+    temp = {}
+    for key, val in attrs.items():
+        if 'params_' in key:
+            new_key = key.split('params_')[1] # get key value for temp dictionary
+            val = val.split(';')[0] # remove non value related information
+            val_list = val.split(' ') # split value string for further analyze
+            val = val_list[0]
+            temp[new_key] = int(val) if '.' not in val else float(val)
+            if 'step' in val_list: # when it indicate the step
+                step_index = val_list.index('step') + 1 # the index of the value of 'step' is equal to the index of string 'index' + 1
+                step = int(val_list[step_index])
+                temp[new_key + '_step'] = step
+
+        if 'sweep_' in key:
+            val_list = val.split(',')
+            val = val_list[1] # get value
+            new_key = 'sweep_' + val_list[0]
+            temp[new_key] = int(val)
+            new_key += '_dim' # last item is the key to the parameters, such as t, p...
+            temp[new_key] = val_list[-1]
+
+    attrs = {**attrs, **temp}
+    return attrs
+
+def specman_coords(attrs, data: _dnp.DNPData, dims):
+    """Generate coords from specman acquisition parameters
+
+    Args:
+        attrs (dict): Dictionary of specman acqusition parameters
+        data (DNPData Object): The specman data
+        dims (list): the list of current dims
+
+    Returns:
+        tuple: dims and coords
+    """
+    shape = _np.shape(data)
+    length = attrs['sweep_X']
+    new_dims = [attrs['sweep_X_dim']] # new dim name list
+    coords = []
+    for index in range(data.ndim):
+        if index < len(new_dims):
+            dim = new_dims[index]
+            dims[index] = dim
+            start = attrs[dim]
+            step = attrs[dim + '_step']
+            coord = []
+            for i in range(length):
+                coord.append(start + step * i)
+        else:
+            coord = _np.arange(0, shape[index])
+
+        coords.append(_np.array(coord))
+    return dims, coords
+
+
