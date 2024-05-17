891dad036f97867ac161ba115f8c65d1b8451d42
Karl <krieger@bridge12.com>
2024-05-17 10:01:31 -0400

    __pow__operator in ABCData & single unittest

--- examples/02_Analysis/plot_04_reference_spectra.py
+++ examples/02_Analysis/plot_04_reference_spectra.py
@@ -11,7 +11,7 @@ This example demonstrates how to reference spectra using Toluene as an example.
 # %%
 # Load Toluene Spectra
 # -------------------------------
-# Start with importing data and creating the DNPLab workspace. 
+# Start with importing data and creating the DNPLab workspace.
 # The data has been processed and saved in .h5 file.
 
 import numpy as np
@@ -33,7 +33,7 @@ dnp.plt.show()
 # %%
 # Find Toluene Peaks
 # -----------------
-# In the previous example, we have demonstrated how to find peaks. Now, we apply the DNPLab function *find_peaks* to get a DNPData object of all peaks. 
+# In the previous example, we have demonstrated how to find peaks. Now, we apply the DNPLab function *find_peaks* to get a DNPData object of all peaks.
 
 peaks = dnp.find_peaks(data)
 dnp.peak_info(peaks)
@@ -41,10 +41,10 @@ dnp.peak_info(peaks)
 # %%
 # Reference Proton Peak
 # -----------------
-# We can see that chemical shift for proton peak is roughly at 2.79 ppm. 
+# We can see that chemical shift for proton peak is roughly at 2.79 ppm.
 # Let's reference this peak to 7.70 ppm applying the DNPLab function *reference*.
 
-data = dnp.reference(data, old_ref = 2.79, new_ref = 7.70)
+data = dnp.reference(data, old_ref=2.79, new_ref=7.70)
 
 # We can plot the spectra after reference.
 dnp.fancy_plot(data, xlim=[-10, 30], title=sampleTag)
@@ -53,8 +53,7 @@ dnp.plt.show()
 # %%
 # Check the Proton Peak
 # -----------------
-# After we referenced the peaks, we can apply *find_peaks* functions again to get a DNPData object of all peaks. 
+# After we referenced the peaks, we can apply *find_peaks* functions again to get a DNPData object of all peaks.
 
 peaks = dnp.find_peaks(data)
 dnp.peak_info(peaks)
-
