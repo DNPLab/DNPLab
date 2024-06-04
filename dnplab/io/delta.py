2d93354de50125260433ba42925becc91eb54fa9
Thorsten Maly <tmaly@bridge12.com>
2024-06-04 12:06:42 -0400

    Committing current state.

--- dnplab/io/delta.py
+++ dnplab/io/delta.py
@@ -100,7 +100,7 @@ def import_delta_data(path, params, verbose=True):
 
     # Data_Type =  int.from_bytes(file_contents[14:16], byteorder="big")
 
-    Data_Type = [
+    Data_Format = [
         unpack(">B", file_contents[14 + ix : 15 + ix])[0] for ix in range(0, 1, 1)
     ][0]
 
@@ -108,6 +108,13 @@ def import_delta_data(path, params, verbose=True):
     #     unpack(">B", file_contents[24 + ix : 25 + ix])[0] for ix in range(0, 8, 1)
     # ][:Data_Dimension_Number]
 
+    # Data_Axis_Type
+    # Array of 8 enumerations. Each element indicates the type of data for that axis.
+    # Together with Data_Format this values determines the data layout in the data
+    # section. Values are: 0 = None, axis not used, 1 = Real, Axis has real data only
+    # 2 = TPPI, 3 = Complex, Axis has complex data, 4 = Real_Complex, Axis should be
+    # accessed as complex when it is the major axis, accessed as real otherwise. This
+    # is only valid when all axes in use have this setting, 5 = Envelop.
     Data_Axis_Type = [
         unpack(">B", file_contents[24 + ix : 25 + ix])[0] for ix in range(0, 8, 1)
     ]
@@ -135,17 +142,29 @@ def import_delta_data(path, params, verbose=True):
         else:
             params["units"].append("indexed")
 
-    Data_Points = [
+
+
+    #### We need to decide whether we want these parameters as a list or an array. I would
+    # suggest array to be able to do math.
+
+
+    Data_Points = _np.array([
         unpack(">I", file_contents[176 + ix : 180 + ix])[0] for ix in range(0, 32, 4)
-    ]
+    ])
 
-    Data_Offset_Start = [
+    Data_Offset_Start = _np.array([
         unpack(">I", file_contents[208 + ix : 212 + ix])[0] for ix in range(0, 32, 4)
-    ]
+    ])
 
-    Data_Offset_Stop = [
+    Data_Offset_Stop = _np.array([
         unpack(">I", file_contents[240 + ix : 244 + ix])[0] for ix in range(0, 32, 4)
-    ]
+    ])
+
+    Valid_pts = Data_Offset_Stop - Data_Offset_Start + 1
+
+
+
+
 
     Data_Axis_Start = [
         unpack(">d", file_contents[272 + ix : 280 + ix])[0] for ix in range(0, 64, 8)
@@ -177,22 +196,40 @@ def import_delta_data(path, params, verbose=True):
 
     abscissa = []
 
-    for ix in range(Data_Dimension_Number):
+    for k in range(Data_Dimension_Number):
         abscissa.append(
-            _np.linspace(Data_Axis_Start[ix], Data_Axis_Stop[ix], Data_Points[ix])
+            # _np.linspace(Data_Axis_Start[k], Data_Axis_Stop[k], Data_Points[k])
+            _np.linspace(Data_Axis_Start[k], Data_Axis_Stop[k], Valid_pts[k])
         )
 
     file_opened = open(path, "rb")
     file_opened.seek(Data_Start)
 
+
+
+
     # Read data from file
-    if Data_Dimension_Number == 2 and Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 3:
-        read_pts = _np.prod(Data_Points) * 4
-    else:
+    if Data_Dimension_Number == 1:
         read_pts = _np.prod(Data_Points) * 2
+
+    elif Data_Dimension_Number == 2:
+        if Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 1:
+            read_pts = _np.prod(Data_Points) * 2
+
+        elif Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 3:
+            read_pts = _np.prod(Data_Points) * 4
+
+
+    # if Data_Dimension_Number == 2 and Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 3:
+    #     read_pts = _np.prod(Data_Points) * 4
+    # else:
+    #     read_pts = _np.prod(Data_Points) * 2
 
     data = _np.fromfile(file_opened, Endian, read_pts)
     file_opened.close()
+
+
+
 
     # 1D data reshaping
     if Data_Dimension_Number == 1:
@@ -212,45 +249,50 @@ def import_delta_data(path, params, verbose=True):
 
     # 2D data reshaping
     elif Data_Dimension_Number == 2:
-        if Data_Axis_Type[0] == 4 or (
-            Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 1
-        ):
-            print("data: ", _np.shape(data))
+        if Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 1:
+
+
+        # if Data_Axis_Type[0] == 4 or (
+        #     Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 1
+        # ):
 
             data_folded = _np.split(data, 2)[0] - 1j * _np.split(data, 2)[1]
+
+            print("data: ", _np.shape(data))
             print("data_folded: ", _np.shape(data_folded))
 
-            data_shaped = _np.reshape(
-                data_folded,
-                [int(Data_Points[0] / 4), int(Data_Points[1] / 4), 4, 4],
-                order="C",
-            )
 
-            print("data_shaped: ", _np.shape(data_shaped))
+            # data_shaped = _np.reshape(
+            #     data_folded,
+            #     [int(Data_Points[0] / 4), int(Data_Points[1] / 4), 4, 4],
+            #     order="C",
+            # )
 
+            
             temp = _np.reshape(data_folded, (Data_Points[0], Data_Points[1]))
+            temp1 = temp[Data_Offset_Start[0]:Data_Offset_Stop[0]+1, Data_Offset_Start[1]:Data_Offset_Stop[1]+1]
 
-            # temp1 = temp()
-
-            out = temp
+            out = temp1
 
             # print("Shape temp:", _np.shape(temp))
-
             # out = _np.concatenate(_np.concatenate(data_shaped, 1), 1)
 
-        elif Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 3:
-            print("Complex")
-            data_folded = [
-                _np.split(data, 4)[0] - 1j * _np.split(data, 4)[1],
-                _np.split(data, 4)[2] - 1j * _np.split(data, 4)[3],
-            ]
-            for idx in enumerate(data_folded):
-                data_shaped[idx] = _np.reshape(
-                    data_folded[idx],
-                    [int(Data_Points[0] / 32), int(Data_Points[1] / 32), 32, 32],
-                    order="F",
-                )
-                out[idx] = _np.concatenate(_np.concatenate(data_shaped[idx], 1), 1)
+
+
+
+        # elif Data_Axis_Type[0] == 3 and Data_Axis_Type[1] == 3:
+        #     print("Complex")
+        #     data_folded = [
+        #         _np.split(data, 4)[0] - 1j * _np.split(data, 4)[1],
+        #         _np.split(data, 4)[2] - 1j * _np.split(data, 4)[3],
+        #     ]
+        #     for idx in enumerate(data_folded):
+        #         data_shaped[idx] = _np.reshape(
+        #             data_folded[idx],
+        #             [int(Data_Points[0] / 32), int(Data_Points[1] / 32), 32, 32],
+        #             order="F",
+        #         )
+        #         out[idx] = _np.concatenate(_np.concatenate(data_shaped[idx], 1), 1)
 
         else:
             raise ValueError("Data format not recognized")
@@ -259,16 +301,17 @@ def import_delta_data(path, params, verbose=True):
 
     else:
         raise TypeError("Only 1D or 2D are supported")
-
+    
     if verbose == True:
         print("Endian: ", Endian)
         print("Data Dimension Number: ", Data_Dimension_Number)
-        print("Data Type: ", Data_Type)
+        print("Data Format: ", Data_Format)
         print("Data Axis Type: ", Data_Axis_Type)
         print("Data Units: ", Data_Units)
         print("Data Points: ", Data_Points)
         print("Data Offset Start: ", Data_Offset_Start)
         print("Data Offset Stop: ", Data_Offset_Stop)
+        print("Valid points: ", Valid_pts)
         print("Data Axis Start", Data_Axis_Start)
         print("data Axis Stop: ", Data_Axis_Stop)
         print("Base Freq: ", Base_Freq)
