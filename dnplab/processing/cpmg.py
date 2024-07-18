c658d7d4b180db9c98116cb1c394d1768b92ce76
Thorsten Maly <tmaly@bridge12.com>
2024-07-18 12:08:59 -0400

    Updates to CPMG module
    Function to detect time of first echo
    Updated SNR calculation

--- dnplab/processing/cpmg.py
+++ dnplab/processing/cpmg.py
@@ -16,8 +16,85 @@ light_grey = DNPLAB_CONFIG.get("COLORS", "light_grey")
 orange = DNPLAB_CONFIG.get("COLORS", "orange")
 
 
+def cpmg_detect_first_echo(data, region=[0, -1], graphical_output=True):
+    """Find time for first echo
+
+    Args:
+        data (dnpdata):                 CPMG tansient (1D)
+        region (list):                  List with start and end time to look for first echo
+        graphical_output (boolean):     Display graphical output
+
+    Return:
+        t_echo:                         Time of first echo.
+    """
+
+    t = data.coords["t2"]
+    signal = data.values
+    signal = _np.squeeze(signal)
+    dwell_time = _np.diff(t)[0]
+
+    region_start_index = _np.squeeze(_np.where(_np.isclose(t, region[0])))
+    region_stop_index = _np.squeeze(_np.where(_np.isclose(t, region[1])))
+
+    echo_index = _np.argmax(signal[region_start_index:region_stop_index], axis=0)
+
+    t_echo = _np.round(t[echo_index + region_start_index], 1)
+
+    if graphical_output == True:
+
+        _plt.subplot(2, 1, 1)
+        _plt.plot(t, signal, color=light_grey, label="Transient")
+        _plt.plot(
+            t[region_start_index:region_stop_index],
+            signal[region_start_index:region_stop_index],
+            color=dark_green,
+            label="Search Region",
+        )
+        _plt.plot(
+            [t_echo, t_echo],
+            [_np.min(signal), _np.max(signal)],
+            color=orange,
+            linestyle="--",
+        )
+        _plt.title("Transient. First Echo at: " + str(t_echo) + " ns")
+        _plt.xlabel("Time (ns)")
+        _plt.ylabel("Signal (a.u.)")
+        _plt.grid(True)
+        _plt.legend()
+
+        _plt.subplot(2, 2, 3)
+        _plt.plot(
+            t[region_start_index:region_stop_index],
+            signal[region_start_index:region_stop_index],
+            color=dark_green,
+        )
+        _plt.plot(
+            [t_echo, t_echo],
+            [
+                _np.min(signal[region_start_index:region_stop_index]),
+                _np.max(signal[region_start_index:region_stop_index]),
+            ],
+            color=orange,
+            linestyle="--",
+        )
+        _plt.grid(True)
+
+        _plt.tight_layout()
+        _plt.show()
+
+    return t_echo
+
+
 def cpmg_show_integration_region(
-    data, n_echo, t_start, t_period, t_width, alternate=True, graphical_output=True
+    data,
+    n_echo,
+    t_start,
+    t_period,
+    t_width,
+    noise_region=[None, None],
+    alternate=True,
+    graphical_output=True,
+    verbose=False,
 ):
     """
     Display integration regions and calculate integration regions for CPMG trace
@@ -33,7 +110,7 @@ def cpmg_show_integration_region(
     signal = data.values
     signal = _np.squeeze(signal)
 
-    filter_window_length = 10
+    filter_window_length = 20
     filter_polynomial_order = 5
 
     t_echo = _np.linspace(t_start + 0, t_start + (n_echo - 1) * t_period, n_echo)
@@ -69,35 +146,27 @@ def cpmg_show_integration_region(
         else:
             echo_slice[:, index] = signal[index_int_start:index_int_stop]
 
-    # This signal to noise calculation does not seem to be correct. This seems to work
-    # on the actual field sweep spectrum, but not on the individual echoes. Not sure
-    # why this shouldn't work.
+    # Calculate signal to noise ratios
 
-    # Calculate signal to noise ratios for 1st echo and summed up echo
-    echo_slice_cumsum = _np.cumsum(echo_slice, axis=1)
-    echo_max_amplitude = _np.max(echo_slice_cumsum, axis=0)
+    if noise_region[0] == None:
+        noise_region[0] = 0
 
-    # To calculate signal to noise ratio, filter (smooth) signal and subtract filter signal
-    # from signal. Calculate std on remaining signal.
+    if noise_region[1] == None:
+        noise_region[1] = -1
 
-    echo_slice_cumsum_smoothed_real = _savgol_filter(
-        echo_slice_cumsum.real,
-        window_length=filter_window_length,
-        polyorder=filter_polynomial_order,
-        axis=0,
-    )
-    echo_slice_cumsum_smoothed_imag = _savgol_filter(
-        echo_slice_cumsum.imag,
-        window_length=filter_window_length,
-        polyorder=filter_polynomial_order,
-        axis=0,
-    )
-    echo_slice_cumsum_smoothed = (
-        echo_slice_cumsum_smoothed_real + echo_slice_cumsum_smoothed_imag
-    )
+    noise = signal[noise_region[0] : noise_region[1]]
+    noise_std = _np.std(noise)
 
-    signal_noise = _np.std(echo_slice_cumsum - echo_slice_cumsum_smoothed, axis=0)
-    snr = echo_max_amplitude / signal_noise
+    if verbose == True:
+        print("Noise value (std): ", noise_std)
+
+    signal = _np.sum(_np.cumsum(echo_slice, axis=0), axis=0) / len(echo_slice[:, 0])
+    signal_cumsum = _np.cumsum(signal)
+
+    snr = signal_cumsum.real / noise_std
+
+    n = _np.linspace(1, n_echo, n_echo)
+    snr = (1 / _np.sqrt(n)) * snr / snr[0]
 
     if graphical_output == True:
 
@@ -140,12 +209,13 @@ def cpmg_show_integration_region(
         _plt.grid(True)
 
         # Plot Results
-        # _plt.subplot(2,2,4)
-        # _plt.plot(_np.linspace(1, n_echo, n_echo), snr.real,".-")
-        # _plt.title("Signal to Noise")
-        # _plt.xlabel("Number of Echoes")
-        # _plt.ylabel("SNR")
-        # _plt.grid(True)
+        _plt.subplot(2, 2, 4)
+        _plt.plot(_np.linspace(1, n_echo, n_echo), snr.real, ".-")
+        # _plt.plot(echo_slice_smoothed[0])
+        _plt.title("Relative SNR Increase")
+        _plt.xlabel("Number of Echoes")
+        _plt.ylabel("SNR")
+        _plt.grid(True)
 
         _plt.tight_layout()
         _plt.show()
